6f7fa0253c42036fe53edbf6c4af61da4d16755a
chless <chlessling@gmail.com>
2024-01-09 15:23:55 +0900

    [bash] mol2text finetuning bash

---
+++ finetune_mol2text.sh
@@ -0,0 +1,20 @@
+[ -z "${task}" ] && task=mol2text
+[ -z "${model}" ] && model="biot5_base"
+[ -z "${log_path}" ] && log_path="path_to_your_log"
+[ -z "${n_node}" ] && n_node=1
+[ -z "${n_gpu_per_node}" ] && n_gpu_per_node=4
+
+cd biot5
+
+export TOKENIZERS_PARALLELISM=false
+
+torchrun --nnodes=${n_node} --nproc_per_node=${n_gpu_per_node} main.py \
+    mode=ft \
+    task=${task} \
+    model=${model} \
+    data=${task} \
+    optim=${finetuning} \
+    molecule_dict=dict/selfies_dict.txt \
+    hydra.run.dir=${log_path} \
+    seed=42 \
+    pred.every_steps=100 logging.every_steps=100 \
