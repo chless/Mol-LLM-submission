4929b7913185b98fe3f1725b225e2e4948c9cdb2
chless <chlessling@gmail.com>
2024-01-14 20:06:56 +0900

    [gpu device] manage visible device

--- finetune_mol2text.sh
+++ finetune_mol2text.sh
@@ -2,19 +2,20 @@
 [ -z "${model}" ] && model="biot5_base"
 [ -z "${log_path}" ] && log_path="logs"
 [ -z "${n_node}" ] && n_node=1
-[ -z "${n_gpu_per_node}" ] && n_gpu_per_node=1
+[ -z "${n_gpu_per_node}" ] && n_gpu_per_node=6
+[ -z "${devices}" ] && devices=0,1,2,3,4,5
 
 cd biot5
 
 export TOKENIZERS_PARALLELISM=false
 
-torchrun --nnodes=${n_node} --nproc_per_node=${n_gpu_per_node} main.py \
+CUDA_VISIBLE_DEVICES=${devices}  torchrun --nnodes=${n_node} --nproc_per_node=${n_gpu_per_node} main.py \
     mode=ft \
     task=${task} \
-    model=${model} \
     data=${task} \
-    optim=finetuning \
+    model=${model} \
+    optim=finetuning_a100_80gb \
     molecule_dict=dict/selfies_dict.txt \
     hydra.run.dir=${log_path} \
     seed=42 \
-    pred.every_steps=100 logging.every_steps=100 \
+    pred.every_steps=1000 logging.every_steps=100 \
