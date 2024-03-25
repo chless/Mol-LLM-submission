5a521d3e92d64fab7f599a86fbb778c3f1d5edb5
chless <chlessling@gmail.com>
2024-03-25 13:33:47 +0900

    [refactor] modify bash script

--- MolCA/script/stage2_graph_embedding_logging_0325.sh
+++ MolCA/script/stage2_graph_embedding_logging_0325.sh
@@ -2,7 +2,7 @@
 python3 MolCA/stage2.py \
 --root 'MolCA/data/PubChem324kV2/' \
 --devices '2,3,4,5,6,7' \
---filename "stage2_graph_embedding_mse" \
+--filename "stage2_graph_embedding_mse_logging" \
 --opt_model 'facebook/galactica-1.3b' \
 --max_epochs 10 \
 --mode pretrain \
@@ -12,5 +12,4 @@ python3 MolCA/stage2.py \
 --inference_batch_size 4 \
 --caption_eval_epoch 10 \
 --filtered_cid_path MolCA/data/PubChem324k/filtered_pretrain_cids.txt \
---graph_embedding_mse_logging \
---graph_embedding_mse_backprop
+--graph_embedding_mse_logging 
