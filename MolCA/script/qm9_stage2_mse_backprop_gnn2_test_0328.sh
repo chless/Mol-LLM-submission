baf0f3a4782a6762ad778216e94acdf69a7f09db
chless <chlessling@gmail.com>
2024-04-10 14:37:39 +0900

    [bash] add qm9 experiment bashes

--- MolCA/script/qm9_stage2_mse_backprop_gnn2_test_0328.sh
+++ MolCA/script/qm9_stage2_mse_backprop_gnn2_test_0328.sh
@@ -1,40 +1,42 @@
 gpus=$1
-
 python3 MolCA/stage2.py \
 --task 'regression2' \
 --root 'qm9_homo' \
 --devices $gpus \
---filename "qm9_homo_frozen_gnn2" \
+--filename "qm9_homo_stage2_mse_backprop2_test" \
+--stage2_path 'MolCA/all_checkpoints/qm9_homo_stage2_mse_backprop2/last.ckpt' \
 --opt_model 'facebook/galactica-1.3b' \
---mode ft \
+--mode test \
 --prompt '[START_I_SMILES]{}[END_I_SMILES].' \
 --tune_gnn \
---inference_batch_size 8 \
+--inference_batch_size 64 \
 --val_check_interval 500 \
---max_epoch 2
+--max_epochs 2
 
 python3 MolCA/stage2.py \
 --task 'regression2' \
 --root 'qm9_lumo' \
 --devices $gpus \
---filename "qm9_lumo_frozen_gnn2" \
+--filename "qm9_lumo_stage2_mse_backprop2_test" \
+--stage2_path 'MolCA/all_checkpoints/qm9_lumo_stage2_mse_backprop2/last.ckpt' \
 --opt_model 'facebook/galactica-1.3b' \
---mode ft \
+--mode test \
 --prompt '[START_I_SMILES]{}[END_I_SMILES].' \
 --tune_gnn \
---inference_batch_size 8 \
+--inference_batch_size 64 \
 --val_check_interval 500 \
---max_epoch 2
+--max_epochs 2
 
 python3 MolCA/stage2.py \
 --task 'regression2' \
 --root 'qm9_homo_lumo_gap' \
 --devices $gpus \
---filename "qm9_homo_lumo_gap_frozen_gnn2" \
+--filename "qm9_homo_lumo_gap_stage2_mse_backprop2_test" \
+--stage2_path 'MolCA/all_checkpoints/qm9_homo_lumo_gap_stage2_mse_backprop2/last.ckpt' \
 --opt_model 'facebook/galactica-1.3b' \
---mode ft \
+--mode test \
 --prompt '[START_I_SMILES]{}[END_I_SMILES].' \
 --tune_gnn \
---inference_batch_size 8 \
+--inference_batch_size 64 \
 --val_check_interval 500 \
---max_epoch 2
+--max_epochs 2
