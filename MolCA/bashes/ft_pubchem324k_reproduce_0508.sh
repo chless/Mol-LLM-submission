5506e6f4b7aec5a1bc1af9e97653c645549cf5e9
chless <chlessling@gmail.com>
2024-05-08 15:58:57 +0900

    [bash] add exp bashes

--- MolCA/bashes/ft_pubchem324k_reproduce_0508.sh
+++ MolCA/bashes/ft_pubchem324k_reproduce_0508.sh
@@ -5,11 +5,11 @@ python3 MolCA/stage2.py \
 --root 'MolCA/data/PubChem324kV2/' \
 --devices $gpus \
 --filename $filename \
---stage1_path '/home/chanhui-lee/text-mol/MolCA/all_checkpoints/stage1_reproduce_0422/epoch=49.ckpt' \
+--stage2_path '/home/chanhui-lee/text-mol/MolCA/all_checkpoints/MolCA/stage2.ckpt' \
 --opt_model 'facebook/galactica-1.3b' \
---max_epochs 10 \
---mode pretrain \
+--max_epochs 100 \
+--mode ft \
 --prompt '[START_I_SMILES]{}[END_I_SMILES].' \
 --tune_gnn \
---llm_tune freeze \
+--llm_tune lora \
 --inference_batch_size 8 \
