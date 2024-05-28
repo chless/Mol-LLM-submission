a69c20ece28cf1c190f0918dcec160c5e02b61de
chless <chlessling@gmail.com>
2024-05-28 09:05:39 +0900

    [bash] add experiment bashes

--- MolCA/bashes/ft_bace_ramdom_query_0523.sh
+++ MolCA/bashes/ft_bace_ramdom_query_0523.sh
@@ -1,19 +1,20 @@
 gpus=$1
-filename=$2
+rq=$2
+echo $rq
 
 python3 MolCA/stage3.py \
---root 'tox21' \
+--root 'bace' \
 --devices $gpus \
---filename $filename \
+--filename ft_bach_rq${rq}_0523 \
 --stage2_path 'MolCA/all_checkpoints/MolCA/stage2.ckpt' \
 --opt_model 'facebook/galactica-1.3b' \
---max_epochs 20 \
 --mode ft \
 --prompt '[START_I_SMILES]{}[END_I_SMILES].' \
 --tune_gnn \
 --llm_tune lora \
---inference_batch_size 1 \
---val_check_interval 1 \
+--inference_batch_size 8 \
+--val_check_interval 0.1 \
 --max_epochs 10 \
 --batch_size 64 \
---num_beam 1
+--num_beam 1 \
+--num_random_query_embedding $rq
--- MolCA/bashes/ft_molnet_cls_ramdom_query_0523.sh
+++ MolCA/bashes/ft_molnet_cls_ramdom_query_0523.sh
@@ -1,19 +1,23 @@
-gpus=$1
-filename=$2
+task=$1
+subtask=$2
+gpus=$3
+rq=$4
+echo $rq
 
 python3 MolCA/stage3.py \
---root 'tox21' \
+--root $task \
+--subtask_idx $subtask \
 --devices $gpus \
---filename $filename \
+--filename ft_${task}${subtask}_rq${rq}_0523 \
 --stage2_path 'MolCA/all_checkpoints/MolCA/stage2.ckpt' \
 --opt_model 'facebook/galactica-1.3b' \
---max_epochs 20 \
 --mode ft \
 --prompt '[START_I_SMILES]{}[END_I_SMILES].' \
 --tune_gnn \
 --llm_tune lora \
---inference_batch_size 1 \
---val_check_interval 1 \
+--inference_batch_size 8 \
+--val_check_interval 0.1 \
 --max_epochs 10 \
 --batch_size 64 \
---num_beam 1
+--num_beam 1 \
+--num_random_query_embedding $rq
