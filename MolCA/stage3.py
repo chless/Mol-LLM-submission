38342e9b67ae289839c47a698dc35f9eb51f0b5b
chless <chlessling@gmail.com>
2024-05-28 09:02:53 +0900

    [neptune] change neptune project

--- MolCA/stage3.py
+++ MolCA/stage3.py
@@ -125,7 +125,7 @@ def main(args):
     logger = CSVLogger(save_dir=f"./MolCA/all_checkpoints/{args.filename}/")
     neptune_logger = NeptuneLogger(
         api_key=os.environ.get("NEPTUNE_API_TOKEN"),
-        project="chanhui-lee/text-mol",
+        project=args.neptune_project,
     )
 
     trainer_args = {
@@ -199,6 +199,7 @@ def get_args():
     parser.add_argument("--check_val_every_n_epoch", type=int, default=1)
     parser.add_argument("--task", type=str, default=None)
     parser.add_argument("--val_check_interval", type=float, default=0.1)
+    parser.add_argument("--neptune_project", type=str, default="chless/text-mol")
 
     # added args
     parser.add_argument("--debug", action="store_true", default=False)
