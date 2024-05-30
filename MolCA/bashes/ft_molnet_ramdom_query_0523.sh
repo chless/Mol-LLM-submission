c30905df95ddd7aa19849afaab6863dc87e062da
chless <chlessling@gmail.com>
2024-05-30 13:02:07 +0900

    [bash] add exp bashes

--- MolCA/bashes/ft_molnet_ramdom_query_0523.sh
+++ MolCA/bashes/ft_molnet_ramdom_query_0523.sh
@@ -1,27 +1,23 @@
 task=$1
 subtask=$2
 gpus=$3
-
-rqs=(0 3 6 8)
-for rq in ${rqs[@]}
-do
-    echo $rq
-    python3 MolCA/stage3.py \
-    --root $task \
-    --subtask_idx $subtask \
-    --devices $gpus \
-    --filename ft_${task}${subtask}_rq${rq}_0523 \
-    --stage2_path 'MolCA/all_checkpoints/MolCA/stage2.ckpt' \
-    --opt_model 'facebook/galactica-1.3b' \
-    --mode ft \
-    --prompt '[START_I_SMILES]{}[END_I_SMILES].' \
-    --tune_gnn \
-    --llm_tune lora \
-    --inference_batch_size 8 \
-    --val_check_interval 0.1 \
-    --max_epochs 10 \
-    --batch_size 64 \
-    --num_beam 1 \
-    --num_random_query_embedding $rq \
-    --result_file MolCA/results/molnet_task-specific_rq${rq}_0530.csv 
-done
+rq=$4
+echo $rq
+python3 MolCA/stage3.py \
+--root $task \
+--subtask_idx $subtask \
+--devices $gpus \
+--filename ft_${task}${subtask}_rq${rq}_0523 \
+--stage2_path 'MolCA/all_checkpoints/MolCA/stage2.ckpt' \
+--opt_model 'facebook/galactica-1.3b' \
+--mode ft \
+--prompt '[START_I_SMILES]{}[END_I_SMILES].' \
+--tune_gnn \
+--llm_tune lora \
+--inference_batch_size 8 \
+--val_check_interval 0.1 \
+--max_epochs 10 \
+--batch_size 64 \
+--num_beam 1 \
+--num_random_query_embedding $rq \
+--result_file MolCA/results/molnet_task-specific_rq${rq}_0530.csv 
