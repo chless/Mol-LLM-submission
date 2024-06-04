f3edf945cc205b336460221f2b224f5cc3201082
chless <chlessling@gmail.com>
2024-06-04 13:24:58 +0900

    chore: Update MolCA script for molecule representation ablation

--- MolCA/bashes/ft_molnet_mol_rep_ablation_0604.sh
+++ MolCA/bashes/ft_molnet_mol_rep_ablation_0604.sh
@@ -7,18 +7,18 @@ python3 MolCA/stage3.py \
 --root $task \
 --subtask_idx $subtask \
 --devices $gpus \
---filename ft_${task}${subtask}_${molrep}_0603 \
+--filename ft_${task}${subtask}_${molrep}_0604 \
 --stage2_path 'MolCA/all_checkpoints/MolCA/stage2.ckpt' \
 --opt_model 'facebook/galactica-1.3b' \
 --mode ft \
 --prompt '[START_I_SMILES]{}[END_I_SMILES].' \
 --tune_gnn \
 --llm_tune lora \
---inference_batch_size 8 \
+--inference_batch_size 24 \
 --val_check_interval 0.1 \
 --max_epochs 10 \
 --batch_size 64 \
 --num_beam 1 \
 --mol_representation $molrep \
---result_file MolCA/results/molnet_task-specific_${molrep}_0603.csv \
+--result_file MolCA/results/molnet_task-specific_${molrep}_0604.csv \
 --not_save_model
