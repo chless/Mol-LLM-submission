0dd5074478dbf1a7f46d87809b7686e1892eb3d8
chless <chlessling@gmail.com>
2024-06-28 04:55:05 +0900

    refactor: Update multi-task script for MolCA

--- MolCA/bashes/multi_task_0628.sh
+++ MolCA/bashes/multi_task_0628.sh
@@ -5,17 +5,17 @@ python3 MolCA/stage3.py \
 --devices $gpus \
 --root multi_task \
 --mode multi_task \
---filename multi_task_0628 \
+--filename instruction_tuning_string_only_galac_0628 \
 --stage2_path 'MolCA/all_checkpoints/MolCA/stage2.ckpt' \
 --opt_model 'facebook/galactica-1.3b' \
 --prompt '[START_I_SMILES]{}[END_I_SMILES].' \
 --tune_gnn \
 --llm_tune lora \
---inference_batch_size 8 \
+--inference_batch_size 840 \
 --val_check_interval 0.1 \
---max_epochs 1 \
+--max_epochs 100 \
 --mol_representation string_only \
 --num_beam 1 \
---batch_size 64 \
---debug \
---result_file MolCA/results/multi_task_0627.csv 
+--batch_size 840 \
+--result_file MolCA/results/instruction_tuning_sting_only_galac_0628.csv \
+--save_every_n_epochs 5
