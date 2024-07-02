b3e80516e145fdd1ffab5fd3d149511d67b9da37
chless <chlessling@gmail.com>
2024-07-03 05:27:09 +0900

    feat: bash addded

---
+++ MolCA/bashes/multi_task_test_string+graph_ours_0703.sh
@@ -0,0 +1,24 @@
+export TOKENIZERS_PARALLELISM=false;
+gpus=$1
+batch_size=32
+inference_batch_size=1920
+
+python3 MolCA/stage3.py \
+--devices $gpus \
+--root multi_task \
+--mode test \
+--filename instruction_tuning_string+graph_ours_galac_0701 \
+--stage2_path '/data/ch_ckpt/instruction_tuning_string+graph_ours_galac_0701/step=109000-total_loss=0.990.ckpt' \
+--result_file /data/ch_ckpt/instruction_tuning_string+graph_ours_galac_0701/benchmark_performance.json \
+--opt_model 'facebook/galactica-1.3b' \
+--prompt '[START_I_SMILES]{}[END_I_SMILES].' \
+--tune_gnn \
+--llm_tune lora \
+--batch_size $batch_size \
+--inference_batch_size $inference_batch_size \
+--val_check_interval 0.1 \
+--max_epochs 10 \
+--mol_representation string+graph \
+--num_beam 1 \
+--save_every_n_epochs 1 \
+--raw_data_root MolCA/data/multi_task_dataset
