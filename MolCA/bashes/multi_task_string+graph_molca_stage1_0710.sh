a1851e02c07f770893cbe52e6f0d46f63b7597e4
chless <chlessling@gmail.com>
2024-07-16 00:41:59 +0900

    refactor: Update multi-task script for MolCA with string+graph representation

--- MolCA/bashes/multi_task_string+graph_molca_stage1_0710.sh
+++ MolCA/bashes/multi_task_string+graph_molca_stage1_0710.sh
@@ -6,19 +6,20 @@ inference_batch_size=$3
 python3 MolCA/stage3.py \
 --devices $gpus \
 --root multi_task \
---mode multi_task \
---filename instruction_tuning_string+graph_molca_galac_0630 \
---stage2_path 'MolCA/all_checkpoints/MolCA/stage2.ckpt' \
+--mode ft \
+--filename instruction_tuning_string+graph_molca_galac_0710 \
+--stage1_path /home/chanhui-lee/text-mol/MolCA/all_checkpoints/MolCA/archived/stage1.ckpt \
 --opt_model 'facebook/galactica-1.3b' \
 --prompt '[START_I_SMILES]{}[END_I_SMILES].' \
 --tune_gnn \
 --llm_tune lora \
 --batch_size $batch_size \
 --inference_batch_size $inference_batch_size \
---val_check_interval 1 \
+--val_check_interval 0.1 \
 --max_epochs 10 \
 --mol_representation string+graph \
 --num_beam 1 \
---result_file MolCA/results/instruction_tuning_sting+graph_molca_galac_0630.csv \
+--result_file MolCA/results/instruction_tuning_sting+graph_molca_galac_0710.csv \
 --save_every_n_epochs 1 \
---raw_data_root MolCA/data/multi_task_dataset
+--raw_data_root MolCA/data/multi_task_dataset \
+--get_raw_data
---
+++ MolCA/bashes/multi_task_string_only_scratch_smiles_0715.sh
@@ -0,0 +1,25 @@
+export TOKENIZERS_PARALLELISM=false;
+gpus=$1
+batch_size=$2
+inference_batch_size=$3
+
+python3 MolCA/stage3.py \
+--devices $gpus \
+--root multi_task \
+--mode ft \
+--filename instruction_tuning_string_only_scratch_smiles_galac_0715 \
+--opt_model 'facebook/galactica-1.3b' \
+--prompt '[START_I_SMILES]{}[END_I_SMILES].' \
+--tune_gnn \
+--llm_tune lora \
+--batch_size $batch_size \
+--inference_batch_size $inference_batch_size \
+--val_check_interval 3000 \
+--max_epochs 10 \
+--mol_representation string_only \
+--num_beam 1 \
+--max_len=512 \
+--text_max_len=256 \
+--result_file MolCA/results/instruction_tuning_sting_only_scratch_smiles_galac_0715.csv \
+--save_every_n_epochs 1 \
+--raw_data_root MolCA/data/multi_task_dataset_0715_smiles \
