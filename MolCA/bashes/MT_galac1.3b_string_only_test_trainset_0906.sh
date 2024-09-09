bde27185f3482a1aa61a520b26b1b380da432522
chless <chelssling@gmail.com>
2024-09-09 10:57:16 +0900

    refactor: Update per_device_batch_size in MT_galac1.3b_string_only_0906.sh

--- MolCA/bashes/MT_galac1.3b_string_only_test_trainset_0906.sh
+++ MolCA/bashes/MT_galac1.3b_string_only_test_trainset_0906.sh
@@ -1,18 +1,18 @@
 export TOKENIZERS_PARALLELISM=false;
-gpus='0,1,2,3,4,5,6,7'
+gpus='1,2,3,4,5,6,7'
 
 python3 MolCA/stage3.py \
 --devices $gpus \
 --root multi_task \
---mode ft \
+--mode test \
 --llm_model facebook/galactica-1.3b \
 --tune_llm lora \
 --lora_r 64 \
---per_device_batch_size_trn 6 \
---per_device_batch_size_rea 6 \
---per_device_batch_size_rxn 6 \
---per_device_batch_size_reg 6 \
 --per_device_batch_size_cls 6 \
+--per_device_batch_size_reg 6 \
+--per_device_batch_size_rxn 6 \
+--per_device_batch_size_rea 6 \
+--per_device_batch_size_trn 6 \
 --per_device_inference_batch_size_trn 40 \
 --per_device_inference_batch_size_rea 40 \
 --per_device_inference_batch_size_rxn 40 \
@@ -33,6 +33,11 @@ python3 MolCA/stage3.py \
 --bert_num_hidden_layers 5 \
 --mol_string_randomization_ratio -1 \
 --mol_representation string_only \
---filename MT_galac1.3b_string_only_0906 \
+--filename MT_galac1.3b_string_only_test_trainset_0909 \
 --scheduler None \
+--truncation 1 \
+--padding max_length \
+--test_on_trainset \
+--trainset_resize 4800 \
 --valset_resize 2400 \
+--stage2_path MolCA/all_checkpoints/MT_galac1.3b_string_only_0906/step=177000-train_total_loss=0.000.ckpt
