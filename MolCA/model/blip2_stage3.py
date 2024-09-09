25a1e42a257b924692ee9001478da7809d8c8a90
chless <chlessling@gmail.com>
2024-09-09 09:55:47 +0900

    refactor: Remove commented code in Blip2Stage3

--- MolCA/model/blip2_stage3.py
+++ MolCA/model/blip2_stage3.py
@@ -340,17 +340,18 @@ class Blip2Stage3(pl.LightningModule):
                 batch_size=total_batch_size,
                 sync_dist=False,
             )
-
+            """
             for key in batches.keys():
                 self.evaluation_in_train_step(
                     batch=batches[key],
                     predictions=self.blip2model.llm_tokenizer.batch_decode(
                         outputs[key]["logits"].argmax(dim=-1)
                     ),
-                    logits=outputs[key]["logits"],
+                    logits=outputs[key]["logits"].clone().detach().cpu(),
                 )
             if (self.trainer.global_step + 1) % self.args.val_check_interval == 0:
                 self.on_train_evaluation_end()
+            """
 
             return total_loss
         elif isinstance(batch, list) and len(batch) == 1:
