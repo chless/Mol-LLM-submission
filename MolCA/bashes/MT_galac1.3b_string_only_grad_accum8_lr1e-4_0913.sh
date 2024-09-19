0f7b255949f794a7de7014a636e5bd79559818a1
chless <chlessling@gmail.com>
2024-09-19 10:31:01 +0900

    chore: change exp bash

---
+++ MolCA/bashes/MT_galac1.3b_string_only_grad_accum8_lr1e-4_0913.sh
@@ -0,0 +1,42 @@
+export TOKENIZERS_PARALLELISM=false;
+gpus='0,1,2,3,4,5,6,7'
+
+python3 MolCA/stage3.py \
+--devices $gpus \
+--root multi_task \
+--mode ft \
+--llm_model facebook/galactica-1.3b \
+--tune_llm lora \
+--lora_r 64 \
+--per_device_batch_size_trn 6 \
+--per_device_batch_size_rea 6 \
+--per_device_batch_size_rxn 6 \
+--per_device_batch_size_reg 6 \
+--per_device_batch_size_cls 6 \
+--per_device_inference_batch_size_trn 30 \
+--per_device_inference_batch_size_rea 30 \
+--per_device_inference_batch_size_rxn 30 \
+--per_device_inference_batch_size_reg 60 \
+--per_device_inference_batch_size_cls 60 \
+--val_check_interval 2500 \
+--max_steps 50000 \
+--second_stage_start_step 20000 \
+--num_beam 1 \
+--raw_data_root MolCA/data/multi_task_dataset_0813_selfies \
+--gen_max_len 256 \
+--prompt_max_len 256 \
+--label_max_len 256 \
+--logging_dir MolCA/all_checkpoints \
+--graph_encoder_ckpt MolCA/MoleculeSTM/molecule_model.pth \
+--llava_style 1 \
+--num_query_token 32 \
+--bert_num_hidden_layers 5 \
+--mol_string_randomization_ratio -1 \
+--mol_representation string_only \
+--filename MT_galac1.3b_string_only_grad_accum8_lr1e-4_0913 \
+--scheduler None \
+--valset_resize 2400 \
+--init_lr 1e-4 \
+--accumulate_grad_batches 8 \
+--every_n_train_steps 100 \
+
