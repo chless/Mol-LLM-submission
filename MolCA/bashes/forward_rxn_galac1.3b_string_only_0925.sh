21de1f16b504ef407dc2ca6155d5e0bcae7ee233
chless <chlessling@gmail.com>
2024-09-25 15:54:18 +0000

    Refactor bash script for forward reaction prediction

--- MolCA/bashes/forward_rxn_galac1.3b_string_only_0925.sh
+++ MolCA/bashes/forward_rxn_galac1.3b_string_only_0925.sh
@@ -1,7 +1,8 @@
 export TOKENIZERS_PARALLELISM=false;
-gpus='0,1,2,3,4,5,6,7'
+gpus="'0,1,2,3,4,5,6,7'"
 
 python3 MolCA/stage3.py \
-++devices $gpus \
+++devices=$gpus \
+++filename=forward_rxn_galac1.3b_string_only_0925 \
 data=forward_reaction_prediction \
-trainer=forward_string_only
+trainer=forward_string_only \
