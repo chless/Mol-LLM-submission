3edd54d2fc933bd0a3e4fc991c00c1f251db4ac6
chless <chlessling@gmail.com>
2024-09-30 13:06:07 +0000

    Refactor: Update MolCA stage3 script for retrosynthesis

--- MolCA/bashes/qm9_galac1.3b_string_only_lr1e-4_0930.sh
+++ MolCA/bashes/qm9_galac1.3b_string_only_lr1e-4_0930.sh
@@ -7,5 +7,5 @@ python3 MolCA/stage3.py \
 data=qm9 \
 trainer=vram80gb_lr1e-4 \
 ++trainer.accumulate_grad_batches=3 \
-++trainer.val_check_interval=1800 \
-++ckpt_path=MolCA/all_checkpoints/qm9_galac1.3b_string_only_B388_lr1e-4_0929/last.ckpt
+++trainer.val_check_interval=900 \
+++ckpt_path=MolCA/all_checkpoints/qm9_galac1.3b_string_only_B388_lr1e-4_0929/step=17400-train_total_loss=0.525.ckpt
