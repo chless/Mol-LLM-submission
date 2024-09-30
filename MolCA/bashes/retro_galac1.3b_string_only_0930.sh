1c1f4118ac0b37641693e1b3de0b4c81e2235bd5
chless <chlessling@gmail.com>
2024-09-30 14:10:57 +0900

    Refactor: Update MolCA stage3 script for retrosynthesis

--- MolCA/bashes/retro_galac1.3b_string_only_0930.sh
+++ MolCA/bashes/retro_galac1.3b_string_only_0930.sh
@@ -3,7 +3,7 @@ gpus="'0,1,2,3,4,5,6,7'"
 
 python3 MolCA/stage3.py \
 ++devices=$gpus \
-++filename=retrosynthesis_galac1.3b_string_only_0925 \
-data=retrosynthesis+smol_0925 \
+++filename=retrosynthesis+smol_galac1.3b_string_only_0930 \
+data=retrosynthesis+smol \
 trainer=vram80gb \
 ++ckpt_path="'MolCA/all_checkpoints/retrosynthesis_galac1.3b_string_only_0925/last-v1.ckpt'"
