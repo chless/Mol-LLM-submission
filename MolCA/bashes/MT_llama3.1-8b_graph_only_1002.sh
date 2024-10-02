9aa9337791eca86a3606a55374173c8b9418b82f
chless <chlessling@gmail.com>
2024-10-02 16:17:39 +0000

    Refactor graph embedding injection in Blip2OPT class

---
+++ MolCA/bashes/MT_llama3.1-8b_graph_only_1002.sh
@@ -0,0 +1,10 @@
+export TOKENIZERS_PARALLELISM=false;
+gpus="'0,1,2,3,4,5,6,7'"
+
+python3 MolCA/stage3.py \
+++devices=$gpus \
+++filename=MT_llama3.1-8b_string_only_1002 \
+data=multi_task \
+trainer=8b_80gb \
+++trainer.mol_representation=graph_only \
+llm=llama3.1-8b-instruct
