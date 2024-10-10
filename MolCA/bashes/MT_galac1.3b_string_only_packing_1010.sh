b6aee735918d82ecc3c8af54d306871ac5020970
chless <chlessling@gmail.com>
2024-10-10 16:37:07 +0900

    Refactor batch sizes and inference batch sizes in 8b_80gb_packing.yaml

---
+++ MolCA/bashes/MT_galac1.3b_string_only_packing_1010.sh
@@ -0,0 +1,10 @@
+export TOKENIZERS_PARALLELISM=false;
+gpus="'0,1,2,3,4,5,6,7'"
+
+python3 MolCA/stage3.py \
+++devices=$gpus \
+++filename=MT_galac1.3b_string_only_packing_1010 \
+data=multi_task \
+trainer=8b_80gb_packing \
+++trainer.mol_representation=string_only \
+llm=galactica1.3b
