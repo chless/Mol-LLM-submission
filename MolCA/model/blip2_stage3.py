3858214c03ce48932a0b530e7150458a75d643fb
chless <chlessling@gmail.com>
2024-10-21 06:38:52 +0000

    fix: if statement to differentiate ddp and single process running

--- MolCA/model/blip2_stage3.py
+++ MolCA/model/blip2_stage3.py
@@ -436,7 +436,8 @@ class Blip2Stage3(pl.LightningModule):
             json.dump(self.list_logs, f, ensure_ascii=False, indent=4)
         
         # assure all predictions are saved before the evaluation
-        dist.barrier()
+        if self.num_devices > 1:
+            dist.barrier()
 
         if self.global_rank == 0:
             all_list_logs = {
@@ -514,6 +515,6 @@ class Blip2Stage3(pl.LightningModule):
                     batch_size=self.eval_dataset_losses[dataset]["total_samples"],
                     sync_dist=False,
                 )
-
-        dist.barrier()
-        dist.destroy_process_group()
+        if self.num_devices > 1:
+            dist.barrier()
+            dist.destroy_process_group()
