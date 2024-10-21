85d0da29ed374676562c06a5673fb5d195d3ba62
chless <chlessling@gmail.com>
2024-10-21 23:59:44 +0900

    chore: add exp bash

--- MolCA/bashes/MT_mistral7b_string_only_1017.sh
+++ MolCA/bashes/MT_mistral7b_string_only_1017.sh
@@ -1,10 +1,9 @@
 export TOKENIZERS_PARALLELISM=false;
-gpus="'5'"
+gpus="'0,1,2,3,4,5,6,7'"
 
 python3 MolCA/stage3.py \
 ++devices=$gpus \
-++filename=MT_mistral7b_string_only_1017 \
+++filename=MT_mistral7b_string_only_1021 \
 data=multi_task_extended \
 trainer=mistral_8b_80gb \
-++trainer.mol_representation=string_only \
-llm=mistral-7b-instruct-v0.3
+++trainer.mol_representation=string_only
