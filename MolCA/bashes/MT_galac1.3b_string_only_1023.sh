b9b027694fcf37fbbb33eaa3d5e6f7575c020f81
chless <chlessling@gmail.com>
2024-10-24 23:30:07 +0900

    chore: change exp bash

--- MolCA/bashes/MT_galac1.3b_string_only_1023.sh
+++ MolCA/bashes/MT_galac1.3b_string_only_1023.sh
@@ -6,4 +6,5 @@ python3 MolCA/stage3.py \
 ++filename=MT_galac1.3b_string_only_1023 \
 data=multi_task_extended \
 trainer=galactica1.3b_80gb \
-++trainer.mol_representation=string_only
+++trainer.mol_representation=string_only \
+++ckpt_path=/home/chanhui-lee/text-mol/MolCA/all_checkpoints/MT_galac1.3b_string_only_1022/step=01800-train_total_loss=0.811.ckpt
