30412d18d6d9f20f729428e62164f7bcf8b4a8d5
chless <chlessling@gmail.com>
2024-10-24 00:22:13 +0000

    chore: change bashes

--- MolCA/bashes/MT_galac1.3b_string_only_1023.sh
+++ MolCA/bashes/MT_galac1.3b_string_only_1023.sh
@@ -3,7 +3,7 @@ gpus="'0,1,2,3,4,5,6,7'"
 
 python3 MolCA/stage3.py \
 ++devices=$gpus \
-++filename=MT_galac1.3b_string_only_1022 \
+++filename=MT_galac1.3b_string_only_1023 \
 data=multi_task_extended \
 trainer=galactica1.3b_80gb \
 ++trainer.mol_representation=string_only
--- MolCA/bashes/MT_mistral7b_string_only_1023.sh
+++ MolCA/bashes/MT_mistral7b_string_only_1023.sh
@@ -3,7 +3,7 @@ gpus="'0,1,2,3,4,5,6,7'"
 
 python3 MolCA/stage3.py \
 ++devices=$gpus \
-++filename=MT_mistral7b_string_only_1022 \
+++filename=MT_mistral7b_string_only_1023 \
 data=multi_task_extended \
 trainer=mistral7b_80gb \
 ++trainer.mol_representation=string_only
