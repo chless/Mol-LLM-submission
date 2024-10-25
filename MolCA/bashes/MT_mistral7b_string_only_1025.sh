2cb5a66647376660ad9ee730669fad0ef120bb4e
chless <chlessling@gmail.com>
2024-10-25 20:16:30 +0000

    chore: change exp bash

--- MolCA/bashes/MT_mistral7b_string_only_1025.sh
+++ MolCA/bashes/MT_mistral7b_string_only_1025.sh
@@ -1,10 +1,9 @@
 export TOKENIZERS_PARALLELISM=false;
-gpus="'1,2,3,4,5,6,7'"
+gpus="'0,1,2,3,4,5,6,7'"
 
 python3 MolCA/stage3.py \
 ++devices=$gpus \
-++filename=MT_mistral7b_string_only_left_1025 \
-data=multi_task_extended \
+++filename=MT_mistral7b_string_only_1025 \
+data=multi_task_extendedv2 \
 trainer=mistral7b_80gb \
 ++trainer.mol_representation=string_only \
-++trainer.accumulate_grad_batches=14
