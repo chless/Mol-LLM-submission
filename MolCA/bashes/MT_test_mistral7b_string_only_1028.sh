90dc5b0cbdc70e719d33a232f12e95036088963d
chless <chlessling@gmail.com>
2024-10-28 18:47:35 +0900

    chore: add test bash

---
+++ MolCA/bashes/MT_test_mistral7b_string_only_1028.sh
@@ -0,0 +1,11 @@
+export TOKENIZERS_PARALLELISM=false;
+gpus="'0,1,2,3,4,5,6,7'"
+
+python3 MolCA/stage3.py \
+++devices=$gpus \
+++filename=MT_mistral7b_string_only_1026 \
+data=multi_task_extended-v2 \
+trainer=mistral7b_80gb \
+++trainer.mol_representation=string_only \
+++mode=test \
+++ckpt_path="'/data/ckpts/molllm-ckpt/MT_mistral7b_string_only_1026/step=15800-train_total_loss=0.383.ckpt'" 
