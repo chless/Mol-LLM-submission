563a1c95bb5c8e873afdcf2ca5fac86f6ab4a849
chless <chlessling@gmail.com>
2024-10-28 21:04:07 +0900

    chore: update test bash for 2/3 trained galactica1.3b, mistral7b

--- MolCA/bashes/MT_test_galac1.3b_string_only_1028.sh
+++ MolCA/bashes/MT_test_galac1.3b_string_only_1028.sh
@@ -1,5 +1,5 @@
 export TOKENIZERS_PARALLELISM=false;
-gpus="'0,1,2,3,4,5,6,7'"
+gpus="'0,1,2,3'"
 
 python3 MolCA/stage3.py \
 ++devices=$gpus \
@@ -8,4 +8,4 @@ data=multi_task_extended-v2 \
 trainer=galactica1.3b_80gb \
 ++trainer.mol_representation=string_only \
 ++mode=test \
-++ckpt_path="'/data/ckpts/molllm-ckpt/MT_galac1.3b_string_only_1026/step=15800-train_total_loss=0.383.ckpt'" 
+++ckpt_path="'/data/ckpts/molllm-ckpt/MT_galac1.3b_string_only_1026/step=17600-train_total_loss=0.469.ckpt'" 
