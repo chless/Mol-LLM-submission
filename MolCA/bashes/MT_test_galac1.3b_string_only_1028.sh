5be93e241dfdc5c72fa146f3ed169ebfa253cad2
chless <chlessling@gmail.com>
2024-10-28 23:38:46 +0900

    chore: add test bash

--- MolCA/bashes/MT_test_galac1.3b_string_only_1028.sh
+++ MolCA/bashes/MT_test_galac1.3b_string_only_1028.sh
@@ -8,8 +8,7 @@ data=multi_task_extended-v2 \
 trainer=galactica1.3b_80gb \
 ++trainer.mol_representation=string_only \
 ++mode=test \
-++ckpt_path="'/data/ckpts/molllm-ckpt/MT_galac1.3b_string_only_1026/step=17600-train_total_loss=0.469.ckpt'" \
 ++trainer.logging_dir=/data/text-mol/ \
 ++trainer.selfies_token_path=MolCA/model/selfies_dict.txt \
 ++data.raw_data_root=/data/datasets/multi_task_0927/ \
-++ckpt_path="'/data/ckpts/molllm-ckpt/MT_mistral7b_string_only_1026/step=05900-train_total_loss=0.380.ckpt'"
+++ckpt_path="'/data/ckpts/molllm-ckpt/MT_galac1.3b_string_only_1026/step=17600-train_total_loss=0.469.ckpt'"
