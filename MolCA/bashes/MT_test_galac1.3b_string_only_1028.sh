c93649ab8554bb0f600ef1589a87b89da0359092
chless <chlessling@gmail.com>
2024-10-28 21:20:47 +0900

    chore: change test bash

--- MolCA/bashes/MT_test_galac1.3b_string_only_1028.sh
+++ MolCA/bashes/MT_test_galac1.3b_string_only_1028.sh
@@ -3,7 +3,7 @@ gpus="'0,1,2,3'"
 
 python3 MolCA/stage3.py \
 ++devices=$gpus \
-++filename=MT_galac1.3b_string_only_1026 \
+++filename=MT_test_galac1.3b_string_only_4ep_1028 \
 data=multi_task_extended-v2 \
 trainer=galactica1.3b_80gb \
 ++trainer.mol_representation=string_only \
--- MolCA/bashes/MT_test_mistral7b_string_only_1028.sh
+++ MolCA/bashes/MT_test_mistral7b_string_only_1028.sh
@@ -3,7 +3,7 @@ gpus="'0,1,2,3'"
 
 python3 MolCA/stage3.py \
 ++devices=$gpus \
-++filename=MT_mistral7b_string_only_1026 \
+++filename=MT_test_mistral7b_string_only_2ep_1028 \
 data=multi_task_extended-v2 \
 trainer=mistral7b_80gb \
 ++trainer.mol_representation=string_only \
