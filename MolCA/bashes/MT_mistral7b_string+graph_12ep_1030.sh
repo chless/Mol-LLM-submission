b12f285046c3e02a7397d286341fb02ed9b73551
chless <chlessling@gmail.com>
2024-10-30 09:56:20 +0000

    chore: add and refactor exp bash

--- MolCA/bashes/MT_mistral7b_string+graph_12ep_1030.sh
+++ MolCA/bashes/MT_mistral7b_string+graph_12ep_1030.sh
@@ -3,7 +3,7 @@ gpus="'0,1,2,3,4,5,6,7'"
 
 python3 MolCA/stage3.py \
 ++devices=$gpus \
-++filename=MT_mistral7b_string_only_12ep_1030 \
+++filename=MT_mistral7b_string+graph_12ep_1030 \
 data=multi_task_extended-v2 \
 trainer=mistral7b_80gb \
-++trainer.mol_representation=string_only \
+++trainer.mol_representation=string+graph \
