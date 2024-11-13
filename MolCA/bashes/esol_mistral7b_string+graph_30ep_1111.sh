b197a0d2c7426b68792aa3e8ba23c0ee77126347
chless <chlessling@gmail.com>
2024-11-13 22:59:19 +0900

    chore: add bash

---
+++ MolCA/bashes/esol_mistral7b_string+graph_30ep_1111.sh
@@ -0,0 +1,11 @@
+export TOKENIZERS_PARALLELISM=false;
+gpus="'0,1,2,3,4,5,6,7'"
+
+python3 MolCA/stage3.py \
+++devices=$gpus \
+++filename=esol_mistral7b_string+graph_30ep_1111 \
+data=esol \
+trainer=mistral7b_80gb \
+++trainer.mol_representation=string+graph \
+++trainer.max_epochs=30 \
+++trainer.accumulate_grad_batches=4
