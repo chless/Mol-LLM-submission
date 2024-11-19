6825da027634ed875d3d4a58de73534b23591745
chless <chlessling@gmail.com>
2024-11-19 12:24:21 +0900

    chore: change bash

--- MolCA/bashes/MT_mistral7b_string_only_10ep_1119.sh
+++ MolCA/bashes/MT_mistral7b_string_only_10ep_1119.sh
@@ -3,7 +3,7 @@ gpus="'0,1,2,3,4,5,6,7'"
 
 python3 MolCA/stage3.py \
 ++devices=$gpus \
-++filename=MT_mistral7b_string_only_12ep_1118 \
+++filename=MT_mistral7b_string_only_12ep_1119 \
 data=multi_task_extended-v6 \
 trainer=mistral7b_lorar32_80gb \
 ++trainer.mol_representation=string_only
