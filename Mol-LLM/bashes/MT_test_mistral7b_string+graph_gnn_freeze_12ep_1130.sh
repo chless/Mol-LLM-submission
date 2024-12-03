8c7550438127eb6ef0ab3411acb28ef46e8644ca
chless <chlessling@gmail.com>
2024-12-03 07:41:01 +0000

    chore: add test bashed

--- Mol-LLM/bashes/MT_test_mistral7b_string+graph_gnn_freeze_12ep_1130.sh
+++ Mol-LLM/bashes/MT_test_mistral7b_string+graph_gnn_freeze_12ep_1130.sh
@@ -1,5 +1,5 @@
 export TOKENIZERS_PARALLELISM=false;
-file_name='MT_mistral7b_string_only_12ep_1126'
+file_name='MT_mistral7b_string+graph_gnn_freeze_12ep_1130'
 gpus="'0,1,2,3,4,5,6,7'"
 
 python Mol-LLM/stage3.py \
@@ -9,6 +9,7 @@ data.raw_data_root=/data/data/Mol-LLM-v6 \
 gnn.graph_encoder_ckpt=/data/all_checkpoints/MoleculeSTM/molecule_model.pth \
 trainer.selfies_token_path=Mol-LLM/model/selfies_dict.txt \
 trainer.logging_dir=/data/all_checkpoints \
-ckpt_path="'/data/all_checkpoints/MT_mistral7b_string_only_12ep_1126/last.ckpt'" \
+trainer.mol_representation=string+graph \
+ckpt_path="'/data/all_checkpoints/MT_mistral7b_string+graph_gnn_freeze_12ep_1130/last.ckpt'" \
 mode=test
 
