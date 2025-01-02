2097fe90dd80e69ce1eb646e6dd4ad3dce7a8210
chless <chlessling@gmail.com>
2025-01-03 05:51:31 +0900

    chore: add exp bash

---
+++ Mol-LLM/bashes/PP_mistral7b_string+graph_gnn_freeze_0103.sh
@@ -0,0 +1,16 @@
+export TOKENIZERS_PARALLELISM=false;
+file_name='PP_mistral7b_string+graph_0103'
+gpus="'0,1,2,3'"
+
+python Mol-LLM/stage3.py \
+trainer.devices=$gpus \
+filename=$file_name \
+data=pp-v7.1 \
+data.raw_data_root=/data/data/Mol-LLM-v7.1 \
+gnn.graph_encoder_ckpt=/data/all_checkpoints/MoleculeSTM/molecule_model.pth \
+trainer=mistral7b_80gb \
+trainer.selfies_token_path=Mol-LLM/model/selfies_dict.txt \
+trainer.logging_dir=/data/all_checkpoints \
+trainer.mol_representation=string+graph \
+trainer.max_epochs=4
+
