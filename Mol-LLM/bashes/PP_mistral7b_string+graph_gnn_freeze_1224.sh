7091315c30724e6f2beafb490ce43da765a81826
chless <chlessling@gmail.com>
2025-01-02 17:39:56 +0900

    chore: change bash

--- Mol-LLM/bashes/PP_mistral7b_string+graph_gnn_freeze_1224.sh
+++ Mol-LLM/bashes/PP_mistral7b_string+graph_gnn_freeze_1224.sh
@@ -5,7 +5,7 @@ gpus="'0,1,2,3,4,5,6,7'"
 python Mol-LLM/stage3.py \
 trainer.devices=$gpus \
 filename=$file_name \
-data.raw_data_root=/data/data/Mol-LLM-v6 \
+data.raw_data_root=/data/data/Mol-LLM-v7.1 \
 gnn.graph_encoder_ckpt=/data/all_checkpoints/MoleculeSTM/molecule_model.pth \
 trainer.selfies_token_path=Mol-LLM/model/selfies_dict.txt \
 trainer.logging_dir=/data/all_checkpoints \
