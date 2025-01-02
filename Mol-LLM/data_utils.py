b186504e82e28efce2c3843fb0dfb0f4c5a184b0
chless <chlessling@gmail.com>
2025-01-02 17:41:23 +0900

    feat: molecular size augmentation

--- Mol-LLM/data_utils.py
+++ Mol-LLM/data_utils.py
@@ -7,7 +7,6 @@ import numpy as np
 
 import re
 
-
 CLASSIFICATION_BENCHMARKS = [
     "smol-property_prediction-bbbp",
     "smol-property_prediction-clintox",
@@ -81,8 +80,6 @@ def id2task(task_id):
 
 import selfies as sf
 
-from ogb.utils import smiles2graph
-
 import rdkit.Chem as Chem
 
 
@@ -104,71 +101,272 @@ def get_selfies_from_input_ids(input_ids, tokenizer):
 
 import random
 
+valence_dict = {
+    "H": 1,
+    "He": 0,
+    "B": 3,
+    "C": 4,
+    "N": 3,
+    "O": 2,
+    "F": 1,
+    "Si": 4,
+    "P": 3,
+    "S": 2,
+    "Cl": 1,
+    "As": 3,
+    "Se": 2,
+    "Br": 1,
+    "Te": 2,
+    "I": 1,
+    # Transition metals and other elements can be added here
+}
 
-def inject_random_C_sequence(selfies: list[str], min_r=0.1, max_r=1.0):
-    # inject random C atoms to the selfies
-    # ratio: the ratio of the number of C atoms to the number of atoms in the molecule
-    mol = Chem.MolFromSmiles(sf.decoder(selfies))
+
+def augment_molecular_size(mol, min_r=0.2, max_r=0.9):
     num_atoms = mol.GetNumAtoms()
-    # count number of C
-    # TODO: reducing C case learning
-    num_existing_C = mol.GetNumAtoms(6)
-    min_C_atoms = int(num_atoms * min_r)
-    max_C_atoms = int(num_atoms * max_r)
-    num_C_atoms = np.random.randint(min_C_atoms, max_C_atoms) + 1
-    # convert to smiles
-    C_smiles = "C" * num_C_atoms
-    C_selfies = sf.encoder(C_smiles)
-    # replace random '[C]' in the selfies with C_selfies
-    pattern = r"\[C\]"
-    matches = list(re.finditer(pattern, selfies))
-    num_matches = len(matches)
+    min_atoms = int(num_atoms * min_r)
+    max_atoms = int(num_atoms * max_r)
+    num_changing_atoms = np.random.randint(min_atoms, max_atoms)
 
-    if num_matches > 0:
-        random_index = random.randint(0, num_matches - 1)
+    if np.random.rand() > 0.5:
+        edit_mol = add_atoms_based_on_valence_dict(
+            mol, num_atoms_to_add=num_changing_atoms
+        )
+    else:
+        edit_mol = remove_atoms(mol, num_atoms_to_remove=num_changing_atoms)
 
-        # Get the start position of the match
-        match_start = matches[random_index].start()
-
-        # Modify the string
-        before = selfies[:match_start]  # Include the ']'
-        after = selfies[match_start:]  # The rest after ']'
-        after = after.replace("[C]", C_selfies, 1)  # Replace first '[' with 'None'
-
-        injected_selfies = before + after
-    return injected_selfies
+    return edit_mol
 
 
-def smiles2data(smiles):
-    graph = smiles2graph(smiles)
-    x = torch.from_numpy(graph["node_feat"])
-    edge_index = torch.from_numpy(
-        graph["edge_index"],
-    )
-    edge_attr = torch.from_numpy(graph["edge_feat"])
-    data = Data(x=x, edge_index=edge_index, edge_attr=edge_attr)
-    return data
+def remove_atoms(mol, num_atoms_to_remove):
+    assert (
+        num_atoms_to_remove < mol.GetNumAtoms()
+    ), "num_atoms_to_remove should be less than the number of atoms in the molecule."
+    assert num_atoms_to_remove > 0, "num_atoms_to_remove should be positive."
+
+    sanitized_mol = Chem.RWMol(mol)
+
+    while num_atoms_to_remove > 0:
+        potential_remove_indices = []
+        for atom in sanitized_mol.GetAtoms():
+            # Check if all neighbors are connected by single bonds
+            # to guarantee that the new bond will be single, so that might be kekulizable...
+            all_single_bonds = True
+            for neighbor in atom.GetNeighbors():
+                bond = sanitized_mol.GetBondBetweenAtoms(
+                    atom.GetIdx(), neighbor.GetIdx()
+                )
+                if bond.GetBondType() != Chem.rdchem.BondType.SINGLE:
+                    all_single_bonds = False
+                    break
+            if all_single_bonds:
+                potential_remove_indices.append(atom.GetIdx())
+
+        if not potential_remove_indices:
+            break
+
+        np.random.shuffle(potential_remove_indices)
+
+        change_made = False
+        for atom_index in potential_remove_indices:
+            try:
+                rw_mol = Chem.RWMol(sanitized_mol)
+                rw_mol.RemoveAtom(atom_index)
+                Chem.SanitizeMol(rw_mol)
+                sanitized_mol = rw_mol.GetMol()
+                num_atoms_to_remove -= 1
+                change_made = True
+            except:
+                continue
+
+        if not change_made:
+            break
+
+    out = {
+        "mol": sanitized_mol,
+        "original_num_atoms": mol.GetNumAtoms(),
+        "augmeted_num_atoms": sanitized_mol.GetNumAtoms(),
+        "num_not_fulfilled_changes": num_atoms_to_remove,
+    }
+    return out
 
 
-def convert_selfies_to_graph(selfies: list[str]):
-    smiles = sf.decoder(selfies)
-    graph = smiles2data(smiles)
+def add_atoms_based_on_valence_dict(mol, num_atoms_to_add):
+    assert num_atoms_to_add > 0, "num_atoms_to_add should be positive."
+
+    sanitized_mol = Chem.RWMol(mol)
+    atom_counts = get_unique_atoms_and_counts(sanitized_mol)
+
+    while num_atoms_to_add > 0:
+        potential_bond_indices = []
+        for atom in sanitized_mol.GetAtoms():
+            if atom.GetSymbol() not in valence_dict:
+                continue
+            if atom.GetDegree() < valence_dict[atom.GetSymbol()]:
+                # Check if all neighbors are connected by single bonds
+                # to guarantee that the new bond will be single, so that might be kekulizable...
+                all_single_bonds = True
+                for neighbor in atom.GetNeighbors():
+                    bond = sanitized_mol.GetBondBetweenAtoms(
+                        atom.GetIdx(), neighbor.GetIdx()
+                    )
+                    if bond.GetBondType() != Chem.rdchem.BondType.SINGLE:
+                        all_single_bonds = False
+                        break
+                if all_single_bonds:
+                    potential_bond_indices.append(atom.GetIdx())
+
+        if not potential_bond_indices:
+            break
+
+        np.random.shuffle(potential_bond_indices)
+
+        change_made = False
+        for atom_index in potential_bond_indices:
+            try:
+                rw_mol = Chem.RWMol(sanitized_mol)
+                new_atom = Chem.Atom(sample_atom(atom_counts))
+                new_atom_idx = rw_mol.AddAtom(new_atom)
+                rw_mol.AddBond(
+                    atom_index, new_atom_idx, order=Chem.rdchem.BondType.SINGLE
+                )
+                Chem.SanitizeMol(rw_mol)
+                sanitized_mol = rw_mol.GetMol()
+                atom_counts[new_atom.GetSymbol()] += 1
+                num_atoms_to_add -= 1
+                change_made = True
+            except:
+                continue
+
+        if not change_made:
+            break
+
+    out = {
+        "mol": sanitized_mol,
+        "original_num_atoms": mol.GetNumAtoms(),
+        "augmeted_num_atoms": sanitized_mol.GetNumAtoms(),
+        "num_not_fulfilled_changes": num_atoms_to_add,
+    }
+    return out
+
+
+from collections import Counter
+
+
+def get_unique_atoms_and_counts(molecule):
+    """
+    Get a list of unique atoms and their counts in the given RDKit molecule object.
+
+    Args:
+    molecule (rdkit.Chem.Mol): RDKit molecule object.
+
+    Returns:
+    dict: A dictionary with atom symbols as keys and their counts as values.
+    """
+    # Extract atom symbols from the molecule
+    atom_symbols = [atom.GetSymbol() for atom in molecule.GetAtoms()]
+
+    # Count occurrences of each atom symbol
+    atom_counts = Counter(atom_symbols)
+
+    return dict(atom_counts)
+
+
+def sample_atom(atom_counts):
+    """
+    Sample an atom from the atom_counts with a probability proportional to its count.
+
+    Args:
+    atom_counts (dict): Dictionary of atom symbols and their counts.
+
+    Returns:
+    str: A sampled atom symbol based on its relative abundance.
+    """
+    # Extract atoms and their respective counts
+    atoms = list(atom_counts.keys())
+    counts = list(atom_counts.values())
+
+    # Sample one atom based on the counts as weights
+    sampled_atom = np.random.choice(atoms, p=np.array(counts) / np.sum(counts)).item()
+
+    return sampled_atom
+
+
+def count_atoms(mol, atom_symbol):
+    # Initialize a counter for the atoms
+    atom_count = 0
+
+    # Iterate over all atoms in the molecule
+    for atom in mol.GetAtoms():
+        if atom.GetSymbol() == atom_symbol:
+            atom_count += 1
+
+    return atom_count
+
+
+def augment_molecular_structure(selfies: list[str], min_r=0.1, max_r=1.0):
+
+    return
+
+
+from ogb.utils.features import atom_to_feature_vector, bond_to_feature_vector
+
+
+def mol2graph(mol):
+    """
+    Converts SMILES string to graph Data object
+    :input: SMILES string (str)
+    :return: graph object
+    """
+
+    # atoms
+    atom_features_list = []
+    for atom in mol.GetAtoms():
+        atom_features_list.append(atom_to_feature_vector(atom))
+    x = np.array(atom_features_list, dtype=np.int64)
+
+    # bonds
+    num_bond_features = 3  # bond type, bond stereo, is_conjugated
+    if len(mol.GetBonds()) > 0:  # mol has bonds
+        edges_list = []
+        edge_features_list = []
+        for bond in mol.GetBonds():
+            i = bond.GetBeginAtomIdx()
+            j = bond.GetEndAtomIdx()
+
+            edge_feature = bond_to_feature_vector(bond)
+
+            # add edges in both directions
+            edges_list.append((i, j))
+            edge_features_list.append(edge_feature)
+            edges_list.append((j, i))
+            edge_features_list.append(edge_feature)
+
+        # data.edge_index: Graph connectivity in COO format with shape [2, num_edges]
+        edge_index = np.array(edges_list, dtype=np.int64).T
+
+        # data.edge_attr: Edge feature matrix with shape [num_edges, num_edge_features]
+        edge_attr = np.array(edge_features_list, dtype=np.int64)
+
+    else:  # mol has no bonds
+        edge_index = np.empty((2, 0), dtype=np.int64)
+        edge_attr = np.empty((0, num_bond_features), dtype=np.int64)
+
+    graph = dict()
+    graph["edge_index"] = edge_index
+    graph["edge_feat"] = edge_attr
+    graph["node_feat"] = x
+    graph["num_nodes"] = len(x)
+
     return graph
 
 
-def prepare_rejected_graphs(selfies: list[str]):
-    rejected_graphs = []
-    for i in selfies:
-        rejected_selfies = inject_random_C_sequence(i)
-        rejected_graph = convert_selfies_to_graph(rejected_selfies)
-        rejected_graphs.append(rejected_graph)
-    return rejected_graphs
-
-
-def prepare_rejected_selfies(selfies: list[str]):
-    rejected_selfies = []
-
-    return
+def prepare_rejected_mol(mol, preference_type="size"):
+    if preference_type == "size":
+        rejected_mol = augment_molecular_size(mol)
+    elif preference_type == "structure":
+        rejected_mol = augment_molecular_structure(mol)
+    return rejected_mol
 
 
 class DataCollator(DataCollatorForSeq2Seq):
@@ -203,9 +401,53 @@ class DataCollator(DataCollatorForSeq2Seq):
         if return_tensors is None:
             return_tensors = self.return_tensors
         tasks = [task2id(sample.pop("task")) for sample in batch]  # task id
+        task_names = [id2task(task) for task in tasks]  # task name
 
-        prompt_tokenized = [sample["prompt_tokenized"] for sample in batch]
-        target_tokenized = [sample["target_tokenized"] for sample in batch]
+        prompt_text = [sample["prompt_text"] for sample in batch]
+        target_text = [sample["target_text"] for sample in batch]
+        input_mol_strings = [sample["input_mol_string"] for sample in batch]
+
+        if self.mdpo:
+            list_selfies = [
+                i.replace("<SELFIES> ", "").replace(" </SELFIES>", "")
+                for i in input_mol_strings
+            ]
+            list_mol = [Chem.MolFromSmiles(sf.decoder(i)) for i in list_selfies]
+            list_rejected_mol = [
+                prepare_rejected_mol(i, preference_type="size")["mol"] for i in list_mol
+            ]
+            list_rejected_selfies = [
+                sf.encoder(Chem.MolToSmiles(i)) for i in list_rejected_mol
+            ]
+
+            for i in range(len(prompt_text)):
+                assert (
+                    list_selfies[i] in prompt_text[i]
+                ), f"{list_selfies[i]} not in {prompt_text[i]}"
+                prompt_text[i] = prompt_text[i].replace(
+                    list_selfies[i], list_rejected_selfies[i]
+                )
+
+            list_rejected_graph = [mol2graph(i) for i in list_rejected_mol]
+
+            list_rejected_additional_graph = [mol2graph(i) for i in list_rejected_mol]
+
+        prompt_tokenized = self.tokenizer(
+            prompt_text,
+            truncation=True,
+            max_length=self.max_length,
+            padding=False,
+            return_tensors=None,
+            add_special_tokens=False,
+        )
+        target_tokenized = self.tokenizer(
+            target_text,
+            truncation=True,
+            max_length=self.max_length - len(prompt_tokenized["input_ids"]),
+            padding=False,
+            return_tensors=None,
+            add_special_tokens=False,
+        )
 
         if self.use_graph:
             graphs = [
@@ -230,23 +472,20 @@ class DataCollator(DataCollatorForSeq2Seq):
                 for sample in batch
             ]
 
-            if self.mdpo:
-                list_selfies = get_selfies_from_input_ids(
-                    [p["input_ids"] for p in prompt_tokenized], self.tokenizer
-                )
-                rejected_graphs = prepare_rejected_graphs(list_selfies)
-                rejected_additional_graphs = prepare_rejected_graphs(list_selfies)
-
         full_input_ids = [
-            p["input_ids"] + t["input_ids"]
-            for p, t in zip(prompt_tokenized, target_tokenized)
+            p + t
+            for p, t in zip(
+                prompt_tokenized["input_ids"], target_tokenized["input_ids"]
+            )
         ]
         full_attention_mask = [
-            p["attention_mask"] + t["attention_mask"]
-            for p, t in zip(prompt_tokenized, target_tokenized)
+            p + t
+            for p, t in zip(
+                prompt_tokenized["attention_mask"], target_tokenized["attention_mask"]
+            )
         ]
 
-        prompt_length = [len(p["input_ids"]) for p in prompt_tokenized]
+        prompt_length = [len(p) for p in prompt_tokenized["input_ids"]]
         full_input_ids = [f_ids[: self.max_length] for f_ids in full_input_ids]
         full_attention_mask = [
             f_ids[: self.max_length] for f_ids in full_attention_mask
@@ -262,8 +501,8 @@ class DataCollator(DataCollatorForSeq2Seq):
         if not self.train:
             prompt_features = self.tokenizer.pad(
                 {
-                    "input_ids": [p["input_ids"] for p in prompt_tokenized],
-                    "attention_mask": [p["attention_mask"] for p in prompt_tokenized],
+                    "input_ids": [p for p in prompt_tokenized["input_ids"]],
+                    "attention_mask": [p for p in prompt_tokenized["attention_mask"]],
                 },
                 padding=self.padding,
                 pad_to_multiple_of=self.pad_to_multiple_of,
@@ -279,8 +518,8 @@ class DataCollator(DataCollatorForSeq2Seq):
             raise NotImplementedError("padding_side should be left")
 
         labels_ids = torch.full_like(features["input_ids"], self.tokenizer.pad_token_id)
-        for i, target in enumerate(target_tokenized):
-            label = target["input_ids"]
+        for i, target in enumerate(target_tokenized["input_ids"]):
+            label = target
             if prompt_length[i] >= self.max_length:
                 continue
             else:
@@ -306,12 +545,12 @@ class DataCollator(DataCollatorForSeq2Seq):
             features["additional_graphs"] = additional_graphs
 
             if self.mdpo:
-                rejected_graphs = self.graph_collator(rejected_graphs)
-                rejected_additional_graphs = self.graph_collator(
-                    rejected_additional_graphs
+                list_rejected_graph = self.graph_collator(list_rejected_graph)
+                list_rejected_additional_graph = self.graph_collator(
+                    list_rejected_additional_graph
                 )
-                features["rejected_graphs"] = rejected_graphs
-                features["rejected_additional_graphs"] = rejected_additional_graphs
+                features["rejected_graphs"] = list_rejected_graph
+                features["rejected_additional_graphs"] = list_rejected_additional_graph
 
             features["is_mol_token"] = (
                 torch.tensor(features["input_ids"]) == self.tokenizer.mol_token_id
@@ -367,3 +606,38 @@ molecule captioning, molecule generation. \n\n"
     }
 
     return tokenized_result
+
+
+def generate_text(data_point, tokenizer, args, test=False):
+    default_system_prompt = "You are a helpful assistant for molecular chemistry, \
+to address tasks including molecular property classification, \
+molecular property regression, chemical reaction prediction, \
+molecule captioning, molecule generation. \n\n"
+
+    input_text = "[INST] " + default_system_prompt + data_point["input"] + " [/INST] "
+    output_text = data_point["output"]
+
+    def postfix_graph_sequence(match):
+        return match.group(0) + graph_sequence
+
+    graph_sequence = "<GRAPH>" + "<mol>" * args.num_query_token + "</GRAPH>"
+
+    if args.mol_representation == "graph_only":
+        input_text = input_mol_string_pattern.sub(graph_sequence, input_text)
+    elif args.mol_representation == "string+graph":
+        input_text = input_mol_string_pattern.sub(postfix_graph_sequence, input_text)
+
+    input_mol_string = "None"
+    if input_mol_string_pattern.search(input_text):
+        input_mol_string = input_mol_string_pattern.search(input_text).group()
+
+    prompt_text = tokenizer.bos_token + input_text
+    target_text = output_text + " " + tokenizer.eos_token
+
+    text_result = {
+        "prompt_text": prompt_text,
+        "target_text": target_text,
+        "input_mol_string": input_mol_string,  # TODO: later, save rdkit mol object
+    }
+
+    return text_result
