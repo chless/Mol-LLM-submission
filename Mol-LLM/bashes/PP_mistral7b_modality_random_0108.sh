f2084f88ee675c33d6791a087bb0eb4f70d881ad
chless <chlessling@gmail.com>
2025-01-08 01:15:52 +0900

    chore: add and modify exp bash

---
+++ Mol-LLM/bashes/PP_mistral7b_modality_random_0108.sh
@@ -0,0 +1,19 @@
+export TOKENIZERS_PARALLELISM=false;
+file_name='PP_mistral7b_moddality_random_0108'
+gpus="'2,3'"
+
+python Mol-LLM/stage3.py \
+trainer.devices=$gpus \
+filename=$file_name \
+data=pp-v7.1 \
+data.raw_data_root=/data/data/Mol-LLM-v7.1 \
+gnn.graph_encoder_ckpt=/data/all_checkpoints/MoleculeSTM/molecule_model.pth \
+trainer=mistral7b_80gb \
+trainer.selfies_token_path=Mol-LLM/model/selfies_dict.txt \
+trainer.logging_dir=/data/all_checkpoints \
+trainer.mol_representation=string+graph \
+trainer.modality_randomization=true \
+trainer.max_epochs=4 \
+trainer.batch_size=22 \
+trainer.inference_batch_size=44
+
--- Mol-LLM/bashes/PP_mistral7b_modality_random_mdpo_0108.sh
+++ Mol-LLM/bashes/PP_mistral7b_modality_random_mdpo_0108.sh
@@ -1,6 +1,6 @@
 export TOKENIZERS_PARALLELISM=false;
-file_name='PP_mistral7b_string+graph_0103'
-gpus="'0,1,2,3,4,5,6'"
+file_name='PP_mistral7b_modality_random_mdpo_0108'
+gpus="'4,5'"
 
 python Mol-LLM/stage3.py \
 trainer.devices=$gpus \
@@ -8,9 +8,12 @@ filename=$file_name \
 data=pp-v7.1 \
 data.raw_data_root=/data/data/Mol-LLM-v7.1 \
 gnn.graph_encoder_ckpt=/data/all_checkpoints/MoleculeSTM/molecule_model.pth \
-trainer=mistral7b_80gb \
+trainer=mistral7b_80gb_mdpo \
 trainer.selfies_token_path=Mol-LLM/model/selfies_dict.txt \
 trainer.logging_dir=/data/all_checkpoints \
 trainer.mol_representation=string+graph \
-trainer.max_epochs=10
+trainer.modality_randomization=true \
+trainer.max_epochs=4 \
+trainer.batch_size=11 \
+trainer.inference_batch_size=22
 
