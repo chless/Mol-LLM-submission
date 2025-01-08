e8f3f70ec126e6fe147ca3be1ccfee447ce28803
chless <chlessling@gmail.com>
2025-01-08 17:41:42 +0900

    chore: add exp bash

--- Mol-LLM/bashes/PP_mistral7b_modality_random_mdpo_0108.sh
+++ Mol-LLM/bashes/PP_mistral7b_modality_random_mdpo_0108.sh
@@ -1,6 +1,6 @@
 export TOKENIZERS_PARALLELISM=false;
 file_name='PP_mistral7b_modality_random_mdpo_0108'
-gpus="'5,6,7'"
+gpus="'1,2,3,4,5,6,7'"
 
 python Mol-LLM/stage3.py \
 trainer.devices=$gpus \
@@ -13,5 +13,5 @@ trainer.selfies_token_path=Mol-LLM/model/selfies_dict.txt \
 trainer.logging_dir=/data/all_checkpoints \
 trainer.mol_representation=string+graph \
 trainer.modality_randomization=true \
-trainer.max_epochs=4
+trainer.max_epochs=10
 
--- Mol-LLM/bashes/PP_mistral7b_string+graph_0108.sh
+++ Mol-LLM/bashes/PP_mistral7b_string+graph_0108.sh
@@ -1,6 +1,6 @@
 export TOKENIZERS_PARALLELISM=false;
 file_name='PP_mistral7b_string+graph_0108'
-gpus="'2,3,4,5,6,7'"
+gpus="'5,6,7'"
 
 python Mol-LLM/stage3.py \
 trainer.devices=$gpus \
