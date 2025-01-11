ae2b09d503a060b1770b02c773a084210f66e1ff
chless <chlessling@gmail.com>
2025-01-12 02:39:28 +0900

    chore: add exp bashes

--- Mol-LLM/bashes/PP_mistral7b_string+graph_0111.sh
+++ Mol-LLM/bashes/PP_mistral7b_string+graph_0111.sh
@@ -1,18 +1,21 @@
 export TOKENIZERS_PARALLELISM=false;
-file_name='PP_mistral7b_string+graph_0108'
-gpus="'5,6,7'"
+file_name='PP_mistral7b_string+graph_0111'
+gpus="'4,5,6,7'"
 
 python Mol-LLM/stage3.py \
 trainer.devices=$gpus \
 filename=$file_name \
-data=pp-v7.1 \
+data=pp-super-small-v7.1 \
 data.raw_data_root=/data/data/Mol-LLM-v7.1 \
+gnn=moleculeSTM \
 gnn.graph_encoder_ckpt=/data/all_checkpoints/MoleculeSTM/molecule_model.pth \
 trainer=mistral7b_80gb \
+trainer.total_batch_size=256 \
+trainer.modality_randomization=false \
 trainer.selfies_token_path=Mol-LLM/model/selfies_dict.txt \
 trainer.logging_dir=/data/all_checkpoints \
 trainer.mol_representation=string+graph \
-trainer.modality_randomization=false \
-trainer.total_batch_size=256 \
-trainer.max_epochs=4
+trainer.max_epochs=5 \
+trainer.val_check_interval=0.25 \
+trainer.skip_sanity_check=true
 
