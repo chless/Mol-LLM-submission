ac1652f04f08f7001c62eb7215dd2824b4afd4c4
chless <chlessling@gmail.com>
2025-01-11 10:34:19 +0000

    chore: add exp bash

--- Mol-LLM/bashes/PP_mistral7b_modality_random_mdpo_0110.sh
+++ Mol-LLM/bashes/PP_mistral7b_modality_random_mdpo_0110.sh
@@ -1,5 +1,5 @@
 export TOKENIZERS_PARALLELISM=false;
-file_name='PP_mistral7b_modality_random_mdpo_0108'
+file_name='PP_mistral7b_modality_random_mdpo_0110'
 gpus="'0,1,2,3,4,5,6,7'"
 
 python Mol-LLM/stage3.py \
