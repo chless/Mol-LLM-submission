670ab64409db45758ea06e6abecf4471afe584f4
chless <chlessling@gmail.com>
2025-01-13 01:04:37 +0000

    chore: add exp bash

--- Mol-LLM/bashes/PP_mistral7b_modality_random_mdpo_simpo0.5_gnn_freeze_0112.sh
+++ Mol-LLM/bashes/PP_mistral7b_modality_random_mdpo_simpo0.5_gnn_freeze_0112.sh
@@ -1,5 +1,5 @@
 export TOKENIZERS_PARALLELISM=false;
-file_name='PP_mistral7b_string+graph_mdpo_llava_projector_gnn_tuning_0112'
+file_name='PP_mistral7b_modality_random_mdpo_simpo0.5_gnn_freeze_0112'
 gpus="'4,5,6,7'"
 
 python Mol-LLM/stage3.py \
@@ -7,12 +7,12 @@ trainer.devices=$gpus \
 filename=$file_name \
 data=pp-super-small-v7.1 \
 data.raw_data_root=/data/data/Mol-LLM-v7.1 \
-gnn=moleculeSTM \
+gnn=moleculeSTM_freeze \
 gnn.graph_encoder_ckpt=/data/all_checkpoints/MoleculeSTM/molecule_model.pth \
 trainer=mistral7b_80gb_mdpo \
-trainer.projector_type=mlp \
 trainer.total_batch_size=256 \
-trainer.modality_randomization=false \
+trainer.simpo_weight=0.5 \
+trainer.modality_randomization=true \
 trainer.selfies_token_path=Mol-LLM/model/selfies_dict.txt \
 trainer.logging_dir=/data/all_checkpoints \
 trainer.mol_representation=string+graph \
--- Mol-LLM/bashes/PP_mistral7b_random_mdpo_llava_projector_gnn_freeze_0112.sh
+++ Mol-LLM/bashes/PP_mistral7b_random_mdpo_llava_projector_gnn_freeze_0112.sh
@@ -1,23 +1,22 @@
 export TOKENIZERS_PARALLELISM=false;
-file_name='PP_mistral7b_modality_random_mdpo_simpo0.5_0111'
-gpus="'1,2,3,4'"
+file_name='PP_mistral7b_random_mdpo_llava_projector_gnn_freeze_0112'
+gpus="'0,1,2,3'"
 
 python Mol-LLM/stage3.py \
 trainer.devices=$gpus \
 filename=$file_name \
 data=pp-super-small-v7.1 \
 data.raw_data_root=/data/data/Mol-LLM-v7.1 \
-gnn=moleculeSTM \
+gnn=moleculeSTM_freeze \
 gnn.graph_encoder_ckpt=/data/all_checkpoints/MoleculeSTM/molecule_model.pth \
 trainer=mistral7b_80gb_mdpo \
+trainer.projector_type=mlp \
 trainer.total_batch_size=256 \
-trainer.simpo_weight=0.5 \
 trainer.modality_randomization=true \
 trainer.selfies_token_path=Mol-LLM/model/selfies_dict.txt \
 trainer.logging_dir=/data/all_checkpoints \
 trainer.mol_representation=string+graph \
 trainer.max_epochs=5 \
 trainer.val_check_interval=0.25 \
-trainer.skip_sanity_check=true \
-ckpt_path="'/data/all_checkpoints/PP_mistral7b_modality_random_mdpo_simpo0.5_0111/last.ckpt'"
+trainer.skip_sanity_check=true
 
