2f7f6f32ed407c9d66152d2f114a5b464227eee0
chless <chlessling@gmail.com>
2025-01-14 15:15:27 +0000

    chore: add exp bash

---
+++ Mol-LLM/bashes/PP_mistral7b_string+graph_mdpo-graph_gnn_freezing_0114.sh
@@ -0,0 +1,22 @@
+export TOKENIZERS_PARALLELISM=false;
+file_name='PP_mistral7b_string+graph_mdpo-graph_gnn_freezing_0114'
+gpus="'0,1,2,3'"
+
+python Mol-LLM/stage3.py \
+trainer.devices=$gpus \
+filename=$file_name \
+data=pp-super-small-v7.1 \
+data.raw_data_root=/data/data/Mol-LLM-v7.1 \
+gnn=moleculeSTM_freeze \
+gnn.graph_encoder_ckpt=/data/all_checkpoints/MoleculeSTM/molecule_model.pth \
+trainer=mistral7b_80gb_mdpo \
+trainer.simpo_modality=graph \
+trainer.total_batch_size=256 \
+trainer.modality_randomization=false \
+trainer.selfies_token_path=Mol-LLM/model/selfies_dict.txt \
+trainer.logging_dir=/data/all_checkpoints \
+trainer.mol_representation=string+graph \
+trainer.max_epochs=5 \
+trainer.val_check_interval=0.25 \
+trainer.skip_sanity_check=true
+
