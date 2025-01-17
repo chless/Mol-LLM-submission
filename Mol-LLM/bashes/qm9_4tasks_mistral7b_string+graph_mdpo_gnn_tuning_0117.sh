bc2e2bb53840c6346e952a1f82769f08b317a5db
chless <chlessling@gmail.com>
2025-01-17 14:27:02 +0000

    chore: add exp bash

--- Mol-LLM/bashes/qm9_4tasks_mistral7b_string+graph_mdpo_gnn_tuning_0117.sh
+++ Mol-LLM/bashes/qm9_4tasks_mistral7b_string+graph_mdpo_gnn_tuning_0117.sh
@@ -1,12 +1,13 @@
 
 export TOKENIZERS_PARALLELISM=false;
 file_name='qm9_4tasks_mistral7b_string+graph_mdpo_gnn_tuning_0117'
-gpus="'0'"
+gpus="'0,1,2,3,4,5,6,7'"
 
 python Mol-LLM/stage3.py \
 trainer.devices=$gpus \
 filename=$file_name \
 data=qm9_4tasks \
+data.data_tag=qm9_4tasks_offline \
 data.raw_data_root=/data/data/Mol-LLM-v7.1 \
 gnn=moleculeSTM \
 gnn.graph_encoder_ckpt=/data/all_checkpoints/MoleculeSTM/molecule_model.pth \
