50c00709edf5351ba46ef6a182701ce2f65577c0
chless <chlessling@gmail.com>
2025-01-19 14:52:25 +0000

    feat: batch sizes config changed for simpo

--- Mol-LLM/data_module.py
+++ Mol-LLM/data_module.py
@@ -23,9 +23,13 @@ class Stage3DM(LightningDataModule):
         self.mode = mode
         self.num_workers = num_workers
 
-        self.batch_size = args.batch_size
         self.max_length = args.max_length
+        self.batch_size = args.batch_size
         self.inference_batch_size = args.inference_batch_size
+        if self.args.train_simpo:
+            self.batch_size //= 2
+        if self.args.eval_simpo:
+            self.inference_batch_size //= 2
 
         self.mol_representation = args.mol_representation
         self.modality_randomization = args.modality_randomization
