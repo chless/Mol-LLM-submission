0b2cf225bcf5547a6921e98047f531323b4dbd3c
chless <chlessling@gmail.com>
2025-01-21 16:16:27 +0000

    chore: modify exp bash

--- Mol-LLM/bashes/MT_mistral7b_string_only2string+graph_mdpo_0122.sh
+++ Mol-LLM/bashes/MT_mistral7b_string_only2string+graph_mdpo_0122.sh
@@ -16,6 +16,7 @@ trainer=mistral7b_80gb_mdpo-post \
 trainer.selfies_token_path=Mol-LLM/model/selfies_dict.txt \
 trainer.logging_dir=/data/all_checkpoints \
 trainer.mol_representation=string+graph \
-trainer.max_epochs=6 \
 trainer.skip_sanity_check=false \
-pretrained_ckpt_path="'/data/all_checkpoints/string_only-resume-last/epoch=11-val_loss=0.000000.ckpt'"
+pretrained_ckpt_path="'/data/all_checkpoints/string_only-resume-last/epoch=11-val_loss=0.000000.ckpt'" \
+trainer.batch_size=5 \
+trainer.inference_batch_size=5
