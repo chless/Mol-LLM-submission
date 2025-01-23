dd050d49026562a1a62bca772031e641751d8e63
chless <chlessling@gmail.com>
2025-01-23 02:08:33 +0000

    feat: change ckpt saving metric

--- Mol-LLM/stage3.py
+++ Mol-LLM/stage3.py
@@ -74,7 +74,7 @@ def main(cfg):
     callbacks.append(
         ModelCheckpoint(
             dirpath=os.path.join(cfg.logging_dir, cfg.filename),
-            filename="{epoch:02d}-{val_total_loss:.3f}",
+            filename="{epoch:02d}-{step}",
             every_n_epochs=cfg.every_n_epochs,
             save_last=True,
             save_top_k=-1,
