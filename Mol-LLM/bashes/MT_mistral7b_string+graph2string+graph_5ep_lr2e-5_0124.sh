9aac98e459ec398e86aa93f157a3754d0d8f737d
chless <chlessling@gmail.com>
2025-01-26 07:07:24 +0000

    chore: add exp bash

--- Mol-LLM/bashes/MT_mistral7b_string+graph2string+graph_5ep_lr2e-5_0124.sh
+++ Mol-LLM/bashes/MT_mistral7b_string+graph2string+graph_5ep_lr2e-5_0124.sh
@@ -1,6 +1,6 @@
 
 export TOKENIZERS_PARALLELISM=false;
-file_name='MT_mistral7b_string+graph2string+graph_mdpo_5ep_lr_0124'
+file_name='MT_mistral7b_string+graph2string+graph_5ep_lr2e-5_0124'
 gpus="'0,1,2,3,4,5,6,7'"
 
 python Mol-LLM/stage3.py \
@@ -13,13 +13,10 @@ gnn=moleculeSTM \
 gnn.tune_gnn=true \
 gnn.graph_encoder_ckpt=/data/all_checkpoints/MoleculeSTM/molecule_model.pth \
 trainer=mistral7b_80gb_mdpo-post \
-trainer.max_epochs=5 \
 trainer.selfies_token_path=Mol-LLM/model/selfies_dict.txt \
 trainer.logging_dir=/data/all_checkpoints \
 trainer.mol_representation=string+graph \
 trainer.skip_sanity_check=false \
 pretrained_ckpt_path="'/data/all_checkpoints/hug_test_sg_cls/all_checkpoints/hug_test_sg_cls/every_epochs/epoch=11-val_loss=0.000000.ckpt'" \
-find_unused_parameters=true \
-trainer.init_lr=0.00002 \
-trainer.min_lr=0.000002 \
-trainer.warmup_lr=0.000002
+trainer.train_simpo=false \
+trainer.eval_simpo=true
