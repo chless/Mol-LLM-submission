812df23d4bda50edd6069cbb9bdc7878b3af978f
chless <chlessling@gmail.com>
2025-01-28 06:45:11 +0000

    chore: add exp bash

---
+++ Mol-LLM/bashes/mol_llm_mdpo_generalization_presto-reagent_prediction.sh
@@ -0,0 +1,23 @@
+
+export TOKENIZERS_PARALLELISM=false;
+file_name='mol_llm_mdpo_generalization_presto-reagent_prediction'
+gpus="'0,1,2,3,4,5,6,7'"
+
+python Mol-LLM/stage3.py \
+trainer.devices=$gpus \
+filename=$file_name \
+mode=test \
+data=multi_task \
+data.data_tag=presto-reagent_prediction \
+data.raw_data_root=/data/data/Mol-LLM-v7.1 \
+gnn=moleculeSTM \
+gnn.tune_gnn=true \
+gnn.graph_encoder_ckpt=/data/all_checkpoints/MoleculeSTM/molecule_model.pth \
+trainer=mistral7b_80gb_mdpo-post \
+trainer.selfies_token_path=Mol-LLM/model/selfies_dict.txt \
+trainer.logging_dir=/data/all_checkpoints \
+trainer.mol_representation=string+graph \
+trainer.skip_sanity_check=false \
+ckpt_path="'/data/all_checkpoints/MT_mistral7b_string+graph2string+graph_mdpo_5ep_lr_0124/epoch=01-step=4861.ckpt'" \
+trainer.eval_simpo=false \
+trainer.inference_batch_size=7
