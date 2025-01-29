4b17ca88dc39b01270aafa0740bf44a8c38e53eb
chless <chlessling@gmail.com>
2025-01-29 07:21:02 +0000

    chore: update exp bash for recent model

--- Mol-LLM/bashes/mol_llm_wo_mdpo_generalization_presto-retrosynthesis.sh
+++ Mol-LLM/bashes/mol_llm_wo_mdpo_generalization_presto-retrosynthesis.sh
@@ -1,14 +1,14 @@
 
 export TOKENIZERS_PARALLELISM=false;
-file_name='mol_llm_mdpo_generalization_presto-reagent_prediction'
-gpus="'0'"
+file_name='mol_llm_wo_mdpo_generalization_presto-retrosynthesis'
+gpus="'1'"
 
 python Mol-LLM/stage3.py \
 trainer.devices=$gpus \
 filename=$file_name \
 mode=test \
 data=multi_task \
-data.data_tag=presto-reagent_prediction \
+data.data_tag=presto-retrosynthesis \
 data.raw_data_root=/data/data/Mol-LLM-v7.1 \
 gnn=moleculeSTM \
 gnn.tune_gnn=true \
@@ -18,6 +18,6 @@ trainer.selfies_token_path=Mol-LLM/model/selfies_dict.txt \
 trainer.logging_dir=/data/all_checkpoints \
 trainer.mol_representation=string+graph \
 trainer.skip_sanity_check=false \
-ckpt_path="'/data/all_checkpoints/MT_mistral7b_string+graph2string+graph_mdpo_5ep_lr_0124/epoch=03-step=8508.ckpt'" \
+ckpt_path="'/data/all_checkpoints/MT_mistral7b_string+graph2string+graph_5ep_lr2e-5_0124/epoch=03-step=7900.ckpt'" \
 trainer.eval_simpo=false \
-trainer.inference_batch_size=12
+trainer.inference_batch_size=7
