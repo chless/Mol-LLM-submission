9ed09f20a120ef5024e41bbfc9fdd25405e92a34
chless <chlessling@gmail.com>
2025-02-07 13:51:55 +0000

    chore: add exp bash

---
+++ Mol-LLM/bashes/qm9_molpo_lambda1.5_anc0.5_0207-2.sh
@@ -0,0 +1,28 @@
+
+export TOKENIZERS_PARALLELISM=false;
+file_name='qm9_molpo_lambda1.5_anc0.5_0207'
+gpus="'0,1,2,3,4,5,6,7'"
+
+python Mol-LLM/stage3.py \
+trainer.devices=$gpus \
+filename=$file_name \
+mode=post-ft \
+data=multi_task \
+data.data_tag=qm9_0207_augmented \
+data.raw_data_root=/data/data/Mol-LLM-v7.1 \
+gnn=moleculeSTM \
+gnn.graph_encoder_ckpt=/data/all_checkpoints/MoleculeSTM/molecule_model.pth \
+trainer=mistral7b_80gb_mdpo-v2 \
+trainer.selfies_token_path=Mol-LLM/model/selfies_dict.txt \
+trainer.logging_dir=/data/all_checkpoints \
+trainer.mol_representation=string+graph \
+trainer.total_batch_size=256 \
+trainer.max_epochs=10 \
+trainer.val_check_interval=0.25 \
+trainer.skip_sanity_check=true \
+trainer.batch_size=4 \
+trainer.inference_batch_size=7 \
+trainer.anc_chosen_weight=0.5 \
+trainer.anc_reject_weight=0.5 \
+trainer.anc_lambda=1.5 \
+ckpt_path="'/data/all_checkpoints/qm9_molpo_lambda1.5_anc0.5_0207/last.ckpt'"
