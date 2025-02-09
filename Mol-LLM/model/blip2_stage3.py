1ceb603b626e9304c2c321d615fe01132040ab78
chless <chlessling@gmail.com>
2025-02-09 05:53:11 +0000

    feat: clamp arg

--- Mol-LLM/model/blip2_stage3.py
+++ Mol-LLM/model/blip2_stage3.py
@@ -290,10 +290,12 @@ class Blip2Stage3(pl.LightningModule):
         anchor_rejected_losses = -F.logsigmoid(rejected_rewards - self.reject_lambda * self.beta * avg_chosen_rewards)
         anchor_chosen_loss = anchor_chosen_losses.mean()
         anchor_rejected_loss = anchor_rejected_losses.mean()
-        #clamped_anchor_chosen_loss = torch.clamp(anchor_chosen_loss, max=loss_simpo)
-        #clamped_anchor_rejected_loss = torch.clamp(anchor_rejected_loss, max=loss_simpo)
-        clamped_anchor_chosen_loss = anchor_chosen_loss
-        clamped_anchor_rejected_loss = anchor_rejected_loss
+        if self.args.anchor_clamp:
+            clamped_anchor_chosen_loss = torch.clamp(anchor_chosen_loss, max=loss_simpo)
+            clamped_anchor_chosen_loss = torch.clamp(anchor_rejected_loss, max=loss_simpo)
+        else:
+            clamped_anchor_chosen_loss = anchor_chosen_loss
+            clamped_anchor_rejected_loss = anchor_rejected_loss
 
         if self.molpo_weight > 0.0:
             loss = self.sft_weight * sft_loss \
