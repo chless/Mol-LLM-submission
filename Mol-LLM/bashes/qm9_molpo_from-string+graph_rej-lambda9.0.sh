5b85315f10a04a7307ba8d091b9199b5c598eb9a
chless <chlessling@gmail.com>
2025-02-11 02:36:36 +0000

    chore: change bash

--- Mol-LLM/bashes/qm9_molpo_from-string+graph_rej-lambda9.0.sh
+++ Mol-LLM/bashes/qm9_molpo_from-string+graph_rej-lambda9.0.sh
@@ -1,6 +1,6 @@
 
 export TOKENIZERS_PARALLELISM=false;
-file_name='qm9_molpo_from-string+graph_rej-lambda6.0'
+file_name='qm9_molpo_from-string+graph_rej-lambda9.0'
 gpus="'0,1,2,3'"
 
 python Mol-LLM/stage3.py \
@@ -22,5 +22,5 @@ trainer.val_check_interval=0.2 \
 trainer.skip_sanity_check=true \
 trainer.batch_size=4 \
 trainer.inference_batch_size=7 \
-trainer.reject_lambda=6.0 \
+trainer.reject_lambda=9.0 \
 pretrained_ckpt_path="'/data/all_checkpoints/hug_test_sg_cls/all_checkpoints/hug_test_sg_cls/every_epochs/epoch=11-val_loss=0.000000.ckpt'"
