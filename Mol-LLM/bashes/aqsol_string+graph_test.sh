03305051335e88a3c7af7758d668348aa595b01c
chless <chlessling@gmail.com>
2025-02-12 05:10:17 +0000

    chore: add exp bash

---
+++ Mol-LLM/bashes/aqsol_string+graph_test.sh
@@ -0,0 +1,21 @@
+
+export TOKENIZERS_PARALLELISM=false;
+file_name='aqsol_string+graph_test'
+gpus="'6,7'"
+
+python Mol-LLM/stage3.py \
+trainer.devices=$gpus \
+filename=$file_name \
+mode=test \
+data=multi_task \
+data.data_tag=aqsol_0212 \
+data.raw_data_root=/data/data/Mol-LLM-v7.1 \
+gnn=moleculeSTM \
+gnn.graph_encoder_ckpt=/data/all_checkpoints/MoleculeSTM/molecule_model.pth \
+trainer=mistral7b_80gb_mdpo-v2 \
+trainer.selfies_token_path=Mol-LLM/model/selfies_dict.txt \
+trainer.logging_dir=/data/all_checkpoints \
+trainer.mol_representation=string+graph \
+trainer.train_simpo=false \
+trainer.eval_simpo=false \
+ckpt_path="'/data/all_checkpoints/hug_test_sg_cls/all_checkpoints/hug_test_sg_cls/every_epochs/epoch=11-val_loss=0.000000.ckpt'"
