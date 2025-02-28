c7dc0310466a6f337076848d435b43a755d11262
chless <chlessling@gmail.com>
2025-02-28 06:20:59 +0000

    chore: add exp bash

--- Mol-LLM/bashes/qm9_pcqm_augmented_string+graph.sh
+++ Mol-LLM/bashes/qm9_pcqm_augmented_string+graph.sh
@@ -1,18 +1,26 @@
+
 export TOKENIZERS_PARALLELISM=false;
-file_name='MT_test_mistral7b_string+graph_12ep_0224'
+file_name='qm9_pcqm_augmented_string+graph'
 gpus="'0,1,2,3,4,5,6,7'"
 
 python Mol-LLM/stage3.py \
 trainer.devices=$gpus \
 filename=$file_name \
-data.data_tag=augmented \
+mode=ft \
+data=multi_task \
+data.data_tag=qm9_pcqm_augmented_0219 \
 data.raw_data_root=/data/data/Mol-LLM-v7.1 \
 gnn=moleculeSTM \
 gnn.graph_encoder_ckpt=/data/all_checkpoints/MoleculeSTM/molecule_model.pth \
-trainer=mistral7b_80gb \
+trainer=mistral7b_80gb_mdpo-v2 \
 trainer.selfies_token_path=Mol-LLM/model/selfies_dict.txt \
 trainer.logging_dir=/data/all_checkpoints \
 trainer.mol_representation=string+graph \
-ckpt_path="'/data/all_checkpoints/hug_test_sg_cls/all_checkpoints/hug_test_sg_cls/every_epochs/epoch=11-val_loss=0.000000.ckpt'" \
-trainer.skip_sanity_check=false
-
+trainer.total_batch_size=256 \
+trainer.max_epochs=15 \
+trainer.val_check_interval=0.25 \
+trainer.skip_sanity_check=true \
+trainer.batch_size=11 \
+trainer.inference_batch_size=22 \
+trainer.train_simpo=false \
+trainer.eval_simpo=false
