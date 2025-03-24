3597e674d462c9a8afc13864bd842a09921ad402
chless <chlessling@gmail.com>
2025-03-24 12:27:33 +0000

    FEAT: save and load trained gnn parameters

--- Mol-LLM/stage3.py
+++ Mol-LLM/stage3.py
@@ -72,6 +72,7 @@ def main(cfg):
             dirpath=os.path.join(cfg.logging_dir, cfg.filename),
             filename="{epoch:02d}-{step}",
             every_n_epochs=cfg.every_n_epochs,
+            every_n_train_steps=cfg.every_n_train_steps,
             save_last=True,
             save_top_k=-1,
         )
@@ -137,6 +138,9 @@ def main(cfg):
         ckpt = torch.load(cfg.pretrained_ckpt_path, map_location="cpu")
         model.load_state_dict(ckpt["state_dict"], strict=False)
         print(f"loaded pretrained model from {cfg.pretrained_ckpt_path}")
+        if "gnn_state_dict" in ckpt.keys() and 'graph' in cfg.mol_representation:
+            model.blip2model.graph_encoder.load_state_dict(ckpt["gnn_state_dict"])
+            print("=========loaded gnn state dict from checkpoint=========")
 
     if cfg.mode in {"ft"}:
         trainer.fit(model, datamodule=dm, ckpt_path=cfg.ckpt_path)
