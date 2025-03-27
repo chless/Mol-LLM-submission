76fc18eb25c84e0941e6b8314ae709ac8af4d8f5
chless <chlessling@gmail.com>
2025-03-27 05:42:12 +0000

    chore: chance exp bash

--- Mol-LLM/bashes/task_specific_training_small_12ep_0318.sh
+++ Mol-LLM/bashes/task_specific_training_small_12ep_0318.sh
@@ -2,6 +2,7 @@ export TOKENIZERS_PARALLELISM=false;
 gpus=$1
 projector_type=$2
 modality=$3
+task=$4
 max_epochs=50
 total_batch_size=32
 
@@ -13,23 +14,21 @@ tasks=(
     "smol-property_prediction-lipo"
 )
 
-for task in "${tasks[@]}"; do
-    echo "==============Executing task: $task==============="
-    python Mol-LLM/stage3.py \
-    trainer.devices=$gpus \
-    filename=${task}_${projector_type}_${modality}_12ep_0318 \
-    data.data_tag=${task}_0219 \
-    data.raw_data_root=/data/data/Mol-LLM-v7.1 \
-    gnn=moleculeSTM \
-    gnn.graph_encoder_ckpt=/data/all_checkpoints/MoleculeSTM/molecule_model.pth \
-    trainer=mistral7b_80gb \
-    trainer.max_epochs=${max_epochs} \
-    trainer.selfies_token_path=Mol-LLM/model/selfies_dict.txt \
-    trainer.logging_dir=/data/all_checkpoints \
-    trainer.mol_representation=${modality} \
-    trainer.projector_type=${projector_type} \
-    trainer.skip_sanity_check=false \
-    trainer.total_batch_size=${total_batch_size} \
-    trainer.every_n_epochs=0
-done
+echo "==============Executing task: $task==============="
+python Mol-LLM/stage3.py \
+trainer.devices=$gpus \
+filename=${task}_${projector_type}_${modality}_12ep_0318 \
+data.data_tag=${task}_0219 \
+data.raw_data_root=/data/data/Mol-LLM-v7.1 \
+gnn=moleculeSTM \
+gnn.graph_encoder_ckpt=/data/all_checkpoints/MoleculeSTM/molecule_model.pth \
+trainer=mistral7b_80gb \
+trainer.max_epochs=${max_epochs} \
+trainer.selfies_token_path=Mol-LLM/model/selfies_dict.txt \
+trainer.logging_dir=/data/all_checkpoints \
+trainer.mol_representation=${modality} \
+trainer.projector_type=${projector_type} \
+trainer.skip_sanity_check=false \
+trainer.total_batch_size=${total_batch_size} \
+trainer.every_n_epochs=0
 
