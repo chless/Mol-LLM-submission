213832b160aea70668084ac0432f896bb0e5b635
chless <chlessling@gmail.com>
2025-04-01 16:41:56 +0000

    chore: change exp bash

--- Mol-LLM/bashes/MT_molpo_0402.sh
+++ Mol-LLM/bashes/MT_molpo_0402.sh
@@ -8,7 +8,6 @@ trainer.devices=$gpus \
 filename=$file_name \
 mode=ft \
 data=molpo \
-data.data_tag=gap_fixed_molpo_0330 \
 data.raw_data_root=/data/data/Mol-LLM-v7.1 \
 gnn=moleculeSTM \
 gnn.graph_encoder_ckpt=/data/all_checkpoints/MoleculeSTM/molecule_model.pth \
@@ -17,5 +16,5 @@ trainer.selfies_token_path=Mol-LLM/model/selfies_dict.txt \
 trainer.logging_dir=/data/all_checkpoints \
 trainer.mol_representation=string+graph \
 trainer.skip_sanity_check=false \
-trainer.reject_lambda=4.0 \
+trainser.sl_noise_ratio=0.0 \
 pretrained_ckpt_path="'/data/all_checkpoints/MT_mistral7b_string_only_12ep_0224/epoch=11-step=61512.ckpt'"
