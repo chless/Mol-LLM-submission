be4600eb6ece30d27e1826876feed6dfa53ae7a8
chless <chlessling@gmail.com>
2025-04-14 09:20:47 +0000

    Updates training script parameters and filenames
    Renames the script for better version tracking. Modifies parameter order and updates default values for `projector_type`, `gradient_clip_val`, and `gnn`. Adjusts filenames and data tags to reflect new configurations and checkpoints.
    
    Improves clarity and aligns settings with the latest model updates.

--- Mol-LLM/bashes/task_specific_training_small_0414.sh
+++ Mol-LLM/bashes/task_specific_training_small_0414.sh
@@ -1,10 +1,13 @@
 export TOKENIZERS_PARALLELISM=false;
 gpus=$1
-projector_type=$2
-modality=$3
-task=$4
+modality=$2
+task=$3
+projector_type=qformer
 max_epochs=50
 total_batch_size=32
+gradient_clip_val=0.5
+gnn=gine_custom
+graph_encoder_ckpt=/data/all_checkpoints/Custom_gnn_models/GINE/best-model.ckpt
 
 tasks=(
     "smol-property_prediction-bbbp"
@@ -17,8 +20,8 @@ tasks=(
 echo "==============Executing task: $task==============="
 python Mol-LLM/stage3.py \
 trainer.devices=$gpus \
-filename=${task}_${projector_type}_${modality}_12ep_0318 \
-data.data_tag=${task}_0219 \
+filename=${task}_${modality}_${gnn}_0414 \
+data.data_tag=${task}_target_value_0414_molpo \
 data.raw_data_root=/data/data/Mol-LLM-v7.1 \
 gnn=moleculeSTM \
 gnn.graph_encoder_ckpt=/data/all_checkpoints/MoleculeSTM/molecule_model.pth \
