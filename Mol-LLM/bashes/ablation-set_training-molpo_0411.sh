40688cc4dd66d403b1896535bde1fc0d1d69b27f
chless <chlessling@gmail.com>
2025-04-14 07:58:03 +0000

    feat: add ablation set training script for molecule processing

---
+++ Mol-LLM/bashes/ablation-set_training-molpo_0411.sh
@@ -0,0 +1,42 @@
+export TOKENIZERS_PARALLELISM=false;
+gpus=$1
+gnn=gine_custom
+modality=$2
+replace_ratio=0.3
+task=ablation
+gradient_clip_val=0.5
+projector_type=qformer
+max_epochs=12
+total_batch_size=256
+graph_encoder_ckpt=/data/all_checkpoints/Custom_gnn_models/GINE/best-model.ckpt
+
+tasks=(
+    "smol-property_prediction-hiv"
+    "smol-property_prediction-sider"
+    "chebi-20-mol2text"
+    "smol-molecule_captioning"
+    "reagent_prediction"
+    "qm9_homo"
+    "forward_reaction_prediction"
+    "smol-forward_synthesis"
+)
+
+echo "==============Executing task: $task==============="
+python Mol-LLM/stage3.py \
+trainer.devices=$gpus \
+filename=${task}_molpo-replace-${replace_ratio}_${gnn}_grad-clip-${gradient_clip_val}_12ep_0411 \
+data.data_tag=${task}_0411_molpo-replace-${replace_ratio} \
+data.raw_data_root=/data/data/Mol-LLM-v7.1 \
+gnn=${gnn} \
+gnn.graph_encoder_ckpt=${graph_encoder_ckpt} \
+trainer=mistral7b_80gb_molpo \
+trainer.max_epochs=${max_epochs} \
+trainer.selfies_token_path=Mol-LLM/model/selfies_dict.txt \
+trainer.logging_dir=/data/all_checkpoints \
+trainer.mol_representation=${modality} \
+trainer.projector_type=${projector_type} \
+trainer.skip_sanity_check=false \
+trainer.total_batch_size=${total_batch_size} \
+trainer.gradient_clip_val=${gradient_clip_val} \
+trainer.every_n_epochs=0
+
