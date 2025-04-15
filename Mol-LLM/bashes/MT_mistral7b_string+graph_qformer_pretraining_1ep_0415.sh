317ffc58fe8dd3ebdb60b82046501d34e9d85962
chless <chlessling@gmail.com>
2025-04-15 14:13:22 +0000

    Refactors training scripts and adds new pretraining script
    Updates existing training scripts to use a unified data format and revised GNN parameters.
    Adds a new pretraining script for string+graph molecular representations.
    Removes an outdated pretraining script to streamline the codebase.

--- Mol-LLM/bashes/MT_mistral7b_string+graph_qformer_pretraining_1ep_0415.sh
+++ Mol-LLM/bashes/MT_mistral7b_string+graph_qformer_pretraining_1ep_0415.sh
@@ -1,5 +1,5 @@
 export TOKENIZERS_PARALLELISM=false;
-file_name='MT_mistral7b_string+graph_12ep_0415'
+file_name='MT_mistral7b_string+graph_qformer_pretraining_1ep_0415'
 gpus="'0,1,2,3,4,5,6,7'"
 
 python Mol-LLM/stage3.py \
@@ -7,7 +7,7 @@ trainer.devices=$gpus \
 filename=$file_name \
 data=multi_task \
 gnn=gine_tokengt \
-trainer=mistral7b_80gb \
+trainer=mistral7b_80gb_llava_pretraining \
 trainer.mol_representation=string+graph \
 trainer.skip_sanity_check=false
 
--- Mol-LLM/bashes/MT_mistral7b_string_only_12ep_0415.sh
+++ Mol-LLM/bashes/MT_mistral7b_string_only_12ep_0415.sh
@@ -5,13 +5,8 @@ gpus="'0,1,2,3,4,5,6,7'"
 python Mol-LLM/stage3.py \
 trainer.devices=$gpus \
 filename=$file_name \
-data.data_tag=3.3M_0415 \
-data.raw_data_root=/data/data/Mol-LLM-v7.1 \
-gnn=moleculeSTM \
-gnn.graph_encoder_ckpt=/data/all_checkpoints/MoleculeSTM/molecule_model.pth \
+data=multi_task \
 trainer=mistral7b_80gb \
-trainer.selfies_token_path=Mol-LLM/model/selfies_dict.txt \
-trainer.logging_dir=/data/all_checkpoints \
 trainer.mol_representation=string_only \
 trainer.skip_sanity_check=false
 
