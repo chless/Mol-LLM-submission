d80ee44cd2f58325f7dfd6e7fddb633c2c14c755
chless <chlessling@gmail.com>
2025-04-16 08:35:28 +0000

    Renames and updates bash script for GINE TokenGT model
    Renames the script to reflect the inclusion of the GINE TokenGT model.
    Adds a new parameter specifying the path to the pretrained checkpoint.
    
    Improves clarity and supports updated model configurations.

--- Mol-LLM/bashes/MT_mistral7b_string+graph_gine_tokengt_12ep_0415.sh
+++ Mol-LLM/bashes/MT_mistral7b_string+graph_gine_tokengt_12ep_0415.sh
@@ -1,5 +1,5 @@
 export TOKENIZERS_PARALLELISM=false;
-file_name='MT_mistral7b_string+graph_12ep_0415'
+file_name='MT_mistral7b_string+graph_gine_tokengt_12ep_0415'
 gpus="'0,1,2,3,4,5,6,7'"
 
 python Mol-LLM/stage3.py \
@@ -9,5 +9,6 @@ data=multi_task \
 gnn=gine_tokengt \
 trainer=mistral7b_80gb \
 trainer.mol_representation=string+graph \
-trainer.skip_sanity_check=false
+trainer.skip_sanity_check=false \
+pretrained_ckpt_path="'/data/all_checkpoints/MT_mistral7b_string+graph_qformer_gine_tokengt_pretraining_1ep_0415/epoch=00-step=3415.ckpt'"
 
