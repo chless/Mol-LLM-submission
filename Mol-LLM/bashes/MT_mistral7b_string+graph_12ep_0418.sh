1f4c842aa84236f5d15edb2432b9e2a3b7688844
chless <chlessling@gmail.com>
2025-04-19 07:46:43 +0000

    Updates script configurations for model training
    Sets default GPU allocation and GNN type for training
    Enables skipping sanity checks and specifies checkpoint path
    Adds WandB ID for improved experiment tracking

--- Mol-LLM/bashes/MT_mistral7b_string+graph_12ep_0418.sh
+++ Mol-LLM/bashes/MT_mistral7b_string+graph_12ep_0418.sh
@@ -1,15 +1,16 @@
 export TOKENIZERS_PARALLELISM=false;
-gpus=$1
-gnn=$2
-file_name=MT_mistral7b_string+graph_${gnn}_12ep_0418
+gpus="'0,1,2,3,4,5,6,7'"
+gnn=gine_tokengt
+file_name=MT_mistral7b_string+graph_${gnn}_12ep_0415
 
 python Mol-LLM/stage3.py \
 trainer.devices=$gpus \
 filename=$file_name \
 data=multi_task \
-data.data_tag=ablation_0411 \
 gnn=${gnn} \
 trainer=mistral7b_80gb \
 trainer.mol_representation=string+graph \
-trainer.skip_sanity_check=false
+trainer.skip_sanity_check=true \
+ckpt_path="'/data/all_checkpoints/MT_mistral7b_string+graph_gine_tokengt_12ep_0415/epoch=03-step=11955.ckpt'" \
+wandb_id=brmjvggz
 
