85b7548dd4cae9c1ce127521d4adb14d3c45b16a
chless <chlessling@gmail.com>
2025-04-21 06:32:02 +0000

    Adds bash scripts for ablation training workflows
    Introduces two new bash scripts to facilitate ablation training tasks.
    Supports configurable parameters for GPUs, modality, GNN type, and batch size.
    Handles multiple predefined tasks and integrates specific dataset paths and checkpoints.

---
+++ Mol-LLM/bashes/ablation-set_training_0421.sh
@@ -0,0 +1,36 @@
+export TOKENIZERS_PARALLELISM=false;
+gpus=$1
+modality=$2
+gnn=$3
+task=ablation
+max_epochs=3
+total_batch_size=256
+
+tasks=(
+    "smol-property_prediction-bbbp"
+    "smol-property_prediction-clintox"
+    "bace"
+    "smol-property_prediction-esol"
+    "smol-property_prediction-lipo"
+    "smol-property_prediction-hiv"
+    "smol-property_prediction-sider"
+    "chebi-20-mol2text"
+    "reagent_prediction"
+    "qm9_homo"
+    "forward_reaction_prediction"
+)
+
+echo "==============Executing task: $task==============="
+python Mol-LLM/stage3.py \
+trainer.devices=$gpus \
+filename=${task}_${modality}_${gnn}_0421 \
+data.data_tag=${task}_0411 \
+data.raw_data_root=/data/data/Mol-LLM-v7.1 \
+gnn=${gnn} \
+trainer=mistral7b_80gb \
+trainer.max_epochs=${max_epochs} \
+trainer.mol_representation=${modality} \
+trainer.skip_sanity_check=false \
+trainer.total_batch_size=${total_batch_size} \
+pretrained_ckpt_path="'/data/text-mol/all_checkpoints/ablation_graph_only_gine_custom_grad-clip-0.5_12ep_0411/last.ckpt'"
+
