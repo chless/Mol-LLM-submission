0e392a90b35f4e98ad4a3dcea858252f9e5ddf6a
chless <chlessling@gmail.com>
2025-04-21 01:06:30 +0000

    Refines training scripts by removing unused parameters
    Eliminates unused parameters like gradient clipping, graph encoder checkpoint, and projector type from training scripts. Simplifies filename patterns and enhances script clarity by dynamically passing the GNN parameter.
    
    Improves maintainability and reduces script complexity.

--- Mol-LLM/bashes/ablation-set_training_0411.sh
+++ Mol-LLM/bashes/ablation-set_training_0411.sh
@@ -1,13 +1,10 @@
 export TOKENIZERS_PARALLELISM=false;
 gpus=$1
 modality=$2
+gnn=$3
 task=ablation
-projector_type=qformer
 max_epochs=12
 total_batch_size=256
-gradient_clip_val=0.5
-gnn=gine_custom
-graph_encoder_ckpt=/data/all_checkpoints/Custom_gnn_models/GINE/best-model.ckpt
 
 tasks=(
     "smol-property_prediction-bbbp"
@@ -26,19 +23,14 @@ tasks=(
 echo "==============Executing task: $task==============="
 python Mol-LLM/stage3.py \
 trainer.devices=$gpus \
-filename=${task}_${modality}_${gnn}_grad-clip-${gradient_clip_val}_12ep_0411 \
+filename=${task}_${modality}_${gnn}_041`` \
 data.data_tag=${task}_0411 \
 data.raw_data_root=/data/data/Mol-LLM-v7.1 \
 gnn=${gnn} \
-gnn.graph_encoder_ckpt=${graph_encoder_ckpt} \
 trainer=mistral7b_80gb \
 trainer.max_epochs=${max_epochs} \
-trainer.selfies_token_path=Mol-LLM/model/selfies_dict.txt \
-trainer.logging_dir=/data/all_checkpoints \
 trainer.mol_representation=${modality} \
-trainer.projector_type=${projector_type} \
 trainer.skip_sanity_check=false \
 trainer.total_batch_size=${total_batch_size} \
-trainer.gradient_clip_val=${gradient_clip_val} \
 trainer.every_n_epochs=0
 
