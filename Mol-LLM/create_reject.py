f899c9ad52d49d01d4c5532a9e132a85768861b6
chless <chlessling@gmail.com>
2025-04-21 06:28:59 +0000

    Enhances task-specific molecule rejection logic
    Introduces task-aware handling for classification and regression benchmarks in molecule rejection generation. Adds support for task-specific thresholds and candidate selection logic. Updates dataset handling and multiprocessing configurations for improved flexibility and clarity.
    
    Fixes inconsistencies in threshold adjustments and enhances maintainability by removing unused or commented-out code.

--- Mol-LLM/create_reject.py
+++ Mol-LLM/create_reject.py
@@ -16,6 +16,7 @@ from rdkit import Chem
 from rdkit.Chem import AllChem
 from rdkit.DataStructs import TanimotoSimilarity
 
+from data_utils import CLASSIFICATION_BENCHMARKS, REGRESSION_BENCHMARKS, REACTION_BENCHMARKS, MOL2TEXT_BENCHMARKS
 
 task2range = {
     # Classification
@@ -62,6 +63,20 @@ task2range = {
 }
 
 task_threshold = {
+    # Classification
+    'smol-property_prediction-bbbp': {
+        'structure': [0.8, 0.05, 0.1],
+    },
+    'smol-property_prediction-clintox': {
+        'structure': [0.8, 0.05, 0.1],
+    },
+    'smol-property_prediction-hiv': {
+        'structure': [0.8, 0.05, 0.1],
+    },
+    'smol-property_prediction-sider': {
+        'structure': [0.8, 0.05, 0.1],
+    },
+    
     
     # Regression
     'qm9_homo': {
@@ -70,7 +85,6 @@ task_threshold = {
         # 'rdm': [0.97, 0.92, 0.01],  # cosine similarity
         'rdm': [0.9, 0.8, 0.02],  # 1- wasserstein_distance
     },
-    
     'smol-property_prediction-esol': {
         'structure': [0.4, 0.05, 0.1],
     },
@@ -80,10 +94,23 @@ task_threshold = {
 }
 
 
-def clean_target_text(text):
-    for token in ['<|', '|>', '<FLOAT>', '</FLOAT>', '</s>']:
-        text = text.replace(token, '')
-    return float(text.strip())
+def clean_target_text(text, task):
+    if task in REGRESSION_BENCHMARKS:
+        for token in ['<|', '|>', '<FLOAT>', '</FLOAT>', '</s>']:
+            text = text.replace(token, '')
+        target = float(text.strip())
+    elif task in CLASSIFICATION_BENCHMARKS:
+        for token in ['<BOOLEAN>', '</BOOLEAN>', '</s>']:
+            text = text.replace(token, '')
+        text = text.strip()
+        target = 1 if text == 'True' else 0
+    elif task in REACTION_BENCHMARKS:
+        raise NotImplementedError(f"{args.task} is not supported for clean_target_text.")
+    elif task in MOL2TEXT_BENCHMARKS:
+        raise NotImplementedError(f"{args.task} is not supported for clean_target_text.")
+    else:
+        raise NotImplementedError(f"{args.task} is not supported for clean_target_text.")
+    return target
 
 def clean_smiles_text(selfies_text):
     selfies_pattern = re.compile(r"<SELFIES>\s*(.*?)\s*</SELFIES>")
@@ -98,7 +125,7 @@ def group_by_target(dataset, target_range, num_procs=10):
         local_results = []
         for idx in range(start_idx, end_idx):
             data = dataset[idx]
-            value = clean_target_text(data['target_text'])
+            value = clean_target_text(data['target_text'], )
             group_idx = bisect.bisect_left(target_range, value) - 1
             local_results.append((group_idx, idx))
         result_list.extend(local_results)
@@ -140,20 +167,23 @@ def structural_similarity_map(data, dataset, indicies_by_target, target_range, a
     n_reject = args.n_reject
     threshold, min_threshold, th_step = task_threshold[args.task]['structure']
     smiles = clean_smiles_text(data['input_mol_string'])
-    group_idx = bisect.bisect_left(target_range, clean_target_text(data['target_text'])) - 1
-
-    # candi_indicies = list(chain.from_iterable(
-    #     sublist for i, sublist in enumerate(indicies_by_target) if i != group_idx
-    # ))
-    far = len(target_range) // 3
-    candi_indicies = list(chain.from_iterable(
-        sublist for i, sublist in enumerate(indicies_by_target) 
-        if i < group_idx - far or group_idx + far < i
-    ))
+    if args.task in REGRESSION_BENCHMARKS:
+        group_idx = bisect.bisect_left(target_range, clean_target_text(data['target_text'])) - 1
+        
+        far = len(target_range) // 3
+        
+        candi_indicies = list(chain.from_iterable(
+            sublist for i, sublist in enumerate(indicies_by_target) 
+            if i < group_idx - far or group_idx + far < i
+        ))
+    elif args.task in CLASSIFICATION_BENCHMARKS:
+        # 0 : False, 1 : True
+        candi_indicies = indicies_by_target[0] if clean_target_text(data['target_text'], args.task) == 1 \
+                            else indicies_by_target[1]
+    else:
+        raise NotImplementedError(f"{args.task} is not supported for reject molecule generation.")
 
     used_indicies = []
-    init_threshold = threshold
-    total_try_count = 0
 
     for i in range(n_reject):
         try_count = 0
@@ -166,7 +196,9 @@ def structural_similarity_map(data, dataset, indicies_by_target, target_range, a
             sim_score = tanimoto_similarity(smiles, clean_smiles_text(dataset[candi_idx]['input_mol_string']))
             if sim_score > threshold:
                 rejected_data = dataset[candi_idx]
+                
                 data[f"{i}-th_rejected_selfies"] = sf.encoder(clean_smiles_text(dataset[candi_idx]['input_mol_string']))
+                
                 data[f"{i}-th_rejected_x"] = rejected_data["x"]
                 data[f"{i}-th_rejected_edge_index"] = rejected_data["edge_index"]
                 data[f"{i}-th_rejected_edge_attr"] = rejected_data["edge_attr"]
@@ -175,18 +207,7 @@ def structural_similarity_map(data, dataset, indicies_by_target, target_range, a
                 data[f"{i}-th_additional_rejected_edge_attr"] = data['additional_edge_attr']
                 used_indicies.append(candi_idx)
                 break
-            # if try_count > 50:
-            #     threshold = max(threshold - th_step, min_threshold)
-            #     try_count = 0
-            # if total_try_count > 200:
-            #     threshold = min_threshold
-            # if total_try_count > 1000:
-            #     threshold = 0.0
-            # if try_count > 50:
-            #     threshold -= th_step
-            #     try_count = 0
-            # if threshold < min_threshold:  # if reached min threshold, randomly select (easy negative)
-            #     threshold = -10.0
+
             if try_count > 100:
                 threshold -= th_step
                 try_count = 0
@@ -194,14 +215,10 @@ def structural_similarity_map(data, dataset, indicies_by_target, target_range, a
                 threshold = -10.0
             
             try_count += 1
-            total_try_count += 1
     return data
 
 
 def match_noon_size(noon1, noon2):  # crop or padding
-    # max_size = max(len(noon1), len(noon2))
-    # noon1 = np.pad(noon1, (0, max_size - len(noon1)), constant_values=0.0)
-    # noon2 = np.pad(noon2, (0, max_size - len(noon2)), constant_values=0.0)
     min_size = min(len(noon1), len(noon2))
     noon1 = noon1[:min_size]
     noon2 = noon2[:min_size]
@@ -256,6 +273,9 @@ def rdm_similarity_map(data, dataset, indicies_by_target, target_range, args):
             # print(f"sim_score: {sim_score}, threshold: {threshold}, index: {i}, try: {try_count}, total: {total_try_count}")    
             if sim_score > threshold:
                 rejected_data = dataset[candi_idx]
+                
+                data[f"{i}-th_rejected_selfies"] = sf.encoder(clean_smiles_text(dataset[candi_idx]['input_mol_string']))
+                
                 data[f"{i}-th_rejected_x"] = rejected_data["x"]
                 data[f"{i}-th_rejected_edge_index"] = rejected_data["edge_index"]
                 data[f"{i}-th_rejected_edge_attr"] = rejected_data["edge_attr"]
@@ -285,9 +305,8 @@ def rdm_similarity_map(data, dataset, indicies_by_target, target_range, args):
 def main(args):
     print(f"Start creating reject molecule for {args.task}")
 
-    dataset_path = os.path.join(args.dataset_path, args.split, args.task)
-    
-    result_path = os.path.join(args.result_path, args.split, args.reject_method, args.task)
+    dataset_path = os.path.join(args.dataset_dir, args.dataset_path)
+    result_path = os.path.join(dataset_path, args.reject_method)
     os.makedirs(result_path, exist_ok=True)
 
     dataset = load_from_disk(dataset_path)
@@ -297,8 +316,21 @@ def main(args):
     print(f"Start creating reject molecule by {args.reject_method}")
 
     range_cfg = task2range[args.task]
-    target_range = np.arange(range_cfg['start_value'], range_cfg['end_value'] + range_cfg['step_size'], range_cfg['step_size'])
-    indicies_by_target = group_by_target(dataset, target_range)
+    if args.task in REGRESSION_BENCHMARKS:
+        target_range = np.arange(range_cfg['start_value'], range_cfg['end_value'] + range_cfg['step_size'], range_cfg['step_size'])
+        indicies_by_target = group_by_target(dataset, target_range)
+        print(f"Target range: {len(target_range)}, {target_range}")
+    elif args.task in CLASSIFICATION_BENCHMARKS:
+        target_range = None
+        indicies_by_target = [[] for _ in range(2)]
+        for idx, data in enumerate(dataset):
+            value = clean_target_text(data['target_text'], args.task)
+            group_idx = value  # 0 : False, 1 : True
+            indicies_by_target[group_idx].append(idx)
+    else:
+        raise NotImplementedError(f"{args.task} is not supported for reject molecule generation.")
+    
+    
 
     if args.reject_method == 'structure':
         reject_method = structural_similarity_map
@@ -320,7 +352,7 @@ def main(args):
     print("Starting multiprocessing map...")
     new_dataset = dataset.map(
         wrapped_func,
-        num_proc=args.num_proc,
+        num_proc=args.num_procs,
         desc="Generating rejected samples"
     )
 
@@ -328,74 +360,28 @@ def main(args):
     print(f"Saved new dataset with rejected molecules to {result_path}")
     
     
-    # args.dataset_path = os.path.join(args.dataset_path, args.split, args.task)
-    # # args.result_path = os.path.join(args.result_path, args.split, args.reject_method, args.task)
-    # args.result_path = os.path.join(args.result_path, args.split, args.reject_method)
-    # os.makedirs(args.result_path, exist_ok=True)
-    
-    # dataset = load_from_disk(args.dataset_path)
-    
-    # args.result_path = os.path.join(args.result_path, args.task)
-    # n_split = 10
-    # split_size = len(dataset) // n_split
-    # for i in range(n_split):
-        
-    #     save_path = args.result_path + f'{i}'
-        
-    #     start_idx = split_size * i
-    #     end_idx = split_size * (i + 1) if i < n_split - 1 else len(dataset)  # 마지막 조각은 남은 전부
-    #     split_dataset = dataset.select(np.arange(start_idx, end_idx))
-    #     # split_dataset = dataset.select(np.arange(split_size * i , split_size*(i+1), 1))
-    #     print(f"Processing split {i + 1}/{n_split}...")
-    #     print(f"Loaded dataset with {len(split_dataset)} samples.")
-
-    #     range_cfg = task2range[args.task]
-    #     target_range = np.arange(range_cfg['start_value'], range_cfg['end_value'] + range_cfg['step_size'], range_cfg['step_size'])
-    #     indicies_by_target = group_by_target(dataset, target_range)
-
-    #     if args.reject_method == 'structure':
-    #         reject_method = structural_similarity_map
-    #     elif args.reject_method == 'matrix':
-    #         reject_method = matrix_similarity_map
-    #     elif args.reject_method == 'rdm':
-    #         reject_method = rdm_similarity_map
-    #     else:
-    #         raise NotImplementError(f'{args.reject_method} is not supported.')
-        
-    #     # 함수 wrapping
-    #     wrapped_func = partial(
-    #         # structural_similarity_map,
-    #         reject_method,
-    #         dataset=dataset,
-    #         indicies_by_target=indicies_by_target,
-    #         target_range=target_range,
-    #         args=args
-    #     )
-
-    #     print("Starting multiprocessing map...")
-    #     new_dataset = split_dataset.map(
-    #         wrapped_func,
-    #         num_proc=args.num_proc,
-    #         desc="Generating rejected samples"
-    #     )
-
-    #     new_dataset.save_to_disk(save_path)
-    #     print(f"Saved new dataset with rejected molecules to {save_path}")
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--dataset_path", type=str, default='/data/data/nonn', help="Path to the dataset")
-    parser.add_argument("--result_path", type=str, default='/data/data/smilar_data', help="Path to save the result")
+    parser.add_argument('--dataset_dir', type=str, default='/data/data/Mol-LLM-v7.1', help="Path to the dataset directory")
+    parser.add_argument('--dataset_path', type=str, default='mistralai-Mistral-7B-Instruct-v0.3_string+graph_q32_train_qm9_homo_0219', help="Path to the dataset")
     
-    parser.add_argument("--split", type=str, default='test', help="Dataset split")
     parser.add_argument("--task", type=str, default='qm9_homo', help="Task name")
     
     parser.add_argument("--n_reject", type=int, default=12, help="Number of rejected molecules to find")
-    parser.add_argument('--reject_method', type=str, default='rdm', choices=['structure', 'matrix', 'rdm'])
+    parser.add_argument('--reject_method', type=str, default='structure', choices=['structure', 'matrix', 'rdm'])
     
-    parser.add_argument("--num_proc", type=int, default=32, help="Number of processes for multiprocessing")
+    parser.add_argument("--num_procs", type=int, default=32, help="Number of processes for multiprocessing")
     args = parser.parse_args()
 
+    #tasks = ['smol-property_prediction-clintox', 'smol-property_prediction-hiv', 'smol-property_prediction-clintox']
+    tasks = []
     
+    for task in tasks:
+        
+        args.task = task
+        print(args.task)
+        main(args)
+
     main(args)
