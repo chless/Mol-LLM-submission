ddb90eaef4cefebef211b451fd3a47dbd3e31926
chless <chlessling@gmail.com>
2025-04-22 13:11:30 +0000

    REFACTOR: remove not used molca, biot5 codes

--- MolCA/bashes/stage2.sh
+++
@@ -1,6 +0,0 @@
-{
-export TOKENIZERS_PARALLELISM=false;
-
-python stage2.py --root 'data/PubChem324k' --devices '0,1' --filename "stage2" --llm_model 'facebook/galactica-1.3b' --max_epochs 10 --mode pretrain --prompt '[START_I_SMILES]{}[END_I_SMILES].' --tune_gnn --tune_llm freeze --inference_batch_size 4 --caption_eval_epoch 10 --filtered_cid_path data/PubChem324k/filtered_pretrain_cids.txt
-exit
-}
