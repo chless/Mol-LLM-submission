5e6bffac2a4b61a8d0e08c9908408f9704761b0e
chless <chlessling@gmail.com>
2025-04-22 08:25:33 +0000

    fix typo

--- Mol-LLM/bashes/ablation-set_training-sft-additional-3ep_0421.sh
+++ Mol-LLM/bashes/ablation-set_training-sft-additional-3ep_0421.sh
@@ -30,7 +30,7 @@ gnn=${gnn} \
 trainer=mistral7b_80gb \
 trainer.max_epochs=${max_epochs} \
 trainer.mol_representation=${modality} \
-trainser.skip_sanity_check=true \
+trainer.skip_sanity_check=true \
 trainer.validation_before_training=true \
 trainer.total_batch_size=${total_batch_size} \
 pretrained_ckpt_path="'/data/text-mol/all_checkpoints/ablation_string+graph_gine_tokengt_0421/epoch=05-step=15653.ckpt'" \
