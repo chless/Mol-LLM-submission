8037fb2da2bd09417fa986c11acb0965b0840426
chless <chlessling@gmail.com>
2025-04-23 00:42:17 +0000

    fix bash type

--- Mol-LLM/bashes/ablation-set_training-molpo_0423.sh
+++ Mol-LLM/bashes/ablation-set_training-molpo_0423.sh
@@ -31,7 +31,7 @@ gnn=${gnn} \
 trainer=mistral7b_80gb_molpo \
 trainer.max_epochs=${max_epochs} \
 trainer.mol_representation=${modality} \
-trainser.skip_sanity_check=false \
+trainer.skip_sanity_check=false \
 trainer.total_batch_size=${total_batch_size} \
 pretrained_ckpt_path="'/data/all_checkpoints/ablation_string+graph_gine_tokengt_0421/epoch=05-step=15653.ckpt'" \
 trainer.reject_label_mask=true
