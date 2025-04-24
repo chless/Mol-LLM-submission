d1d019d44609e043142d5f1eb64d85c9379ed150
chless <chlessling@gmail.com>
2025-04-24 01:47:10 +0000

    fix pretrained ckpt path in bash

--- Mol-LLM/bashes/MT_mistral7b_string+graph_12ep_0424.sh
+++ Mol-LLM/bashes/MT_mistral7b_string+graph_12ep_0424.sh
@@ -1,7 +1,7 @@
 export TOKENIZERS_PARALLELISM=false;
 gpus="'0,1,2,3,4,5,6,7'"
 gnn=gine_tokengt
-file_name=MT_mistral7b_string+graph_${gnn}_12ep_0423
+file_name=MT_mistral7b_string+graph_${gnn}_12ep_0424
 
 python Mol-LLM/stage3.py \
 trainer.devices=$gpus \
@@ -11,5 +11,5 @@ gnn=${gnn} \
 trainer=mistral7b_80gb \
 trainer.mol_representation=string+graph \
 trainer.skip_sanity_check=false \
-pretrained_ckpt_path="'/data/all_checkpoints/MT_mistral7b_string+graph_gine_qformer_pretraining_1ep_0415/epoch=00-step=3267.ckpt'" \
+pretrained_ckpt_path="'/data/all_checkpoints/MT_mistral7b_string+graph_qformer_${gnn}_pretraining_1ep_0415/epoch=00-step=3415.ckpt'" \
 
