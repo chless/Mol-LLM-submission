44e520666d01efd8ba913466c3753155b6388598
chless <chlessling@gmail.com>
2025-04-25 04:15:21 +0000

    change molpo exp bash

--- Mol-LLM/bashes/ablation-set_training-molpo_0424.sh
+++ Mol-LLM/bashes/ablation-set_training-molpo_0424.sh
@@ -1,10 +1,9 @@
 export TOKENIZERS_PARALLELISM=false;
 gpus=$1
-modality=$2
-gnn=$3
-replace_ratio=0.3
-task=ablation
-max_epochs=12
+anc_reject_clip=$2
+modality=string+graph
+gnn=gine_tokengt
+max_epochs=3
 total_batch_size=256
 
 tasks=(
@@ -24,13 +23,15 @@ tasks=(
 echo "==============Executing task: $task==============="
 python Mol-LLM/stage3.py \
 trainer.devices=$gpus \
-filename=${task}_molpo-replace-${replace_ratio}_0421 \
-data.data_tag=${task}_0411_molpo-replace-${replace_ratio} \
+filename=ablation_molpo-anc_reject_clip-${anc_reject_clip}_additional-3ep_0424 \
+data.data_tag=ablation_0411_molpo-replace-0.3 \
 data.raw_data_root=/data/data/Mol-LLM-v7.1 \
 gnn=${gnn} \
 trainer=mistral7b_80gb_molpo \
 trainer.max_epochs=${max_epochs} \
 trainer.mol_representation=${modality} \
 trainer.skip_sanity_check=false \
-trainer.total_batch_size=${total_batch_size}
+trainer.total_batch_size=${total_batch_size} \
+trainer.anc_reject_clip=${anc_reject_clip} \
+pretrained_ckpt_path="'/data/all_checkpoints/ablation_molpo_reject-v2_0423/epoch=02-step=4872.ckpt'"
 
