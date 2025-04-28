4b3e7c065e3069bde048769bb46d3df15d0831de
chless <chlessling@gmail.com>
2025-04-28 14:39:17 +0000

    refactor exp bash

--- Mol-LLM/bashes/MT_molpo_from-string_only_12ep_0428.sh
+++ Mol-LLM/bashes/MT_molpo_from-string_only_12ep_0428.sh
@@ -2,8 +2,6 @@ export TOKENIZERS_PARALLELISM=false;
 gpus="'0,1,2,3,4,5,6,7'"
 gnn=gine_custom
 file_name=MT_molpo_from-string_only_${gnn}_12ep_0428
-molpo_lambda=$1
-rejected_lambda=$2
 max_epochs=3
 
 python Mol-LLM/stage3.py \
@@ -15,7 +13,7 @@ trainer=mistral7b_80gb_molpo \
 trainer.max_epochs=${max_epochs} \
 trainer.mol_representation=string+graph \
 trainer.skip_sanity_check=false \
-trainer.molpo_lambda=$molpo_lambda \
-trainer.rejected_lambda=$rejected_lambda \
-pretrained_ckpt_path="'/data/all_checkpoints/MT_mistral7b_string+graph_qformer_${gnn}_pretraining_1ep_0415/epoch=00-step=3415.ckpt'"
+pretrained_ckpt_path="'/data/all_checkpoints/MT_mistral7b_string_only_12ep_0415/epoch=09-step=32451.ckpt'" \
+trainer.init_lr=0.00004 \
+trainer.warmup_lr=0.000004
 
