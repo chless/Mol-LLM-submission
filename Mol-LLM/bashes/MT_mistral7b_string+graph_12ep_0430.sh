d607b24174f6025d977c1ca35dacacfd22940707
chless <chlessling@gmail.com>
2025-04-30 03:51:59 +0000

    update exp bash

--- Mol-LLM/bashes/MT_mistral7b_string+graph_12ep_0430.sh
+++ Mol-LLM/bashes/MT_mistral7b_string+graph_12ep_0430.sh
@@ -1,7 +1,7 @@
 export TOKENIZERS_PARALLELISM=false;
 gpus="'0,1,2,3,4,5,6,7'"
-gnn=gine_tokengt
-file_name=MT_mistral7b_string+graph_${gnn}_12ep_0424
+gnn=gine_custom
+file_name=MT_mistral7b_string+graph_${gnn}_12ep_0430
 
 python Mol-LLM/stage3.py \
 trainer.devices=$gpus \
---
+++ Mol-LLM/bashes/MT_string_only_12+6ep_0430.sh
@@ -0,0 +1,20 @@
+export TOKENIZERS_PARALLELISM=false;
+gpus="'0,1,2,3,4,5,6,7'"
+gnn=gine_custom
+file_name=MT_string_only_12+6ep_0430
+max_epochs=6
+gnn=gine_custom
+
+python Mol-LLM/stage3.py \
+trainer.devices=$gpus \
+filename=$file_name \
+data=molpo \
+gnn=${gnn} \
+trainer=mistral7b_80gb \
+trainer.max_epochs=${max_epochs} \
+trainer.mol_representation=string+graph \
+trainer.skip_sanity_check=false \
+pretrained_ckpt_path="'/data/all_checkpoints/MT_mistral7b_string_only_12ep_0415/epoch=11-step=40991.ckpt'" \
+trainer.init_lr=0.00004 \
+trainer.warmup_lr=0.000004
+
