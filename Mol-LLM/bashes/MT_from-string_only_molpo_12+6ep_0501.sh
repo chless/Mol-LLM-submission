907fe21ef79a23e99e20bc9b9bfddcc15ef478df
chless <chlessling@gmail.com>
2025-05-01 06:20:07 +0000

    update exp bash

--- Mol-LLM/bashes/MT_from-string_only_molpo_12+6ep_0501.sh
+++ Mol-LLM/bashes/MT_from-string_only_molpo_12+6ep_0501.sh
@@ -9,7 +9,6 @@ filename=$file_name \
 data=molpo \
 gnn=$gnn \
 trainer=mistral7b_80gb_molpo \
-trainer.max_epochs=${max_epochs} \
 trainer.mol_representation=string+graph \
 trainer.skip_sanity_check=false \
 pretrained_ckpt_path="'/data/all_checkpoints/MT_from-string_only_qformer-${gnn}_pretraining_1ep_0430/last.ckpt'"
