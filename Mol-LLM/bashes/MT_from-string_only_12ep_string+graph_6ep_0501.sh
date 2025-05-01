74ff8eaba94b3b07ada92a63d76df4cfbdc79c39
chless <chlessling@gmail.com>
2025-05-01 06:24:47 +0000

    add exp bash

--- Mol-LLM/bashes/MT_from-string_only_12ep_string+graph_6ep_0501.sh
+++ Mol-LLM/bashes/MT_from-string_only_12ep_string+graph_6ep_0501.sh
@@ -1,7 +1,7 @@
 export TOKENIZERS_PARALLELISM=false;
 gpus="'0,1,2,3,4,5,6,7'"
 gnn=gine_custom
-file_name=MT_string+graph-${gnn}_12+6ep_0501
+file_name=MT_from-string_only_12ep_string+graph-${gnn}_6ep_0501
 max_epochs=6
 
 python Mol-LLM/stage3.py \
@@ -16,5 +16,5 @@ trainer.skip_sanity_check=false \
 pretrained_ckpt_path="'/data/all_checkpoints/MT_mistral7b_string_only_12ep_0415/epoch=11-step=40991.ckpt'" \
 trainer.init_lr=0.00004 \
 trainer.warmup_lr=0.000004 \
-trainer.val_check_interval=0.25
+trainer.val_check_interval=0.20
 
