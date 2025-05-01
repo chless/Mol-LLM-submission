dd662e4663b49b809e95161eedc7da5b5049628c
chless <chelssling@gmail.com>
2025-05-01 04:09:48 +0000

    merge exp bash

--- Mol-LLM/bashes/MT_molpo-gine_from-string_only_12+6ep_0430.sh
+++ Mol-LLM/bashes/MT_molpo-gine_from-string_only_12+6ep_0430.sh
@@ -1,14 +1,16 @@
 export TOKENIZERS_PARALLELISM=false;
 gpus="'0,1,2,3,4,5,6,7'"
-file_name=MT_molpo_from-string_only_12+6ep_0430
+gnn=$1
+file_name=MT_molpo-${gnn}_from-string_only_12+6ep_0430
 
 python Mol-LLM/stage3.py \
 trainer.devices=$gpus \
 filename=$file_name \
 data=molpo \
+gnn=$gnn \
 trainer=mistral7b_80gb_molpo \
 trainer.max_epochs=${max_epochs} \
 trainer.mol_representation=string+graph \
 trainer.skip_sanity_check=false \
-pretrained_ckpt_path="'/data/all_checkpoints/MT_from-string_only_qformer_pretraining_1ep_0430/last.ckpt'"
+pretrained_ckpt_path="'/data/all_checkpoints/MT_from-string_only_qformer-${gnn}_pretraining_1ep_0430/last.ckpt'"
 
