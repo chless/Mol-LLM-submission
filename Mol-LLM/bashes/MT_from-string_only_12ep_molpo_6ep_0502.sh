0a96bd81c94b933b4ef20a270bb9b67b57d8548d
chless <chelssling@gmail.com>
2025-05-02 05:40:34 +0000

    update exp bash

--- Mol-LLM/bashes/MT_from-string_only_12ep_molpo_6ep_0502.sh
+++ Mol-LLM/bashes/MT_from-string_only_12ep_molpo_6ep_0502.sh
@@ -9,5 +9,5 @@ data=molpo \
 trainer=mistral7b_80gb_molpo \
 trainer.mol_representation=string+graph \
 trainer.skip_sanity_check=false \
-pretrained_ckpt_path="'/data/all_checkpoints/MT_from-string_only_qformer-gine_tokengt_pretraining_1ep_0501/epoch=00-step=3415.ckpt'"
+pretrained_ckpt_path="'/data/all_checkpoints/MT_from-string_only_qformer-gine_tokengt_pretraining_1ep_0501/epoch=00-step=3415_lora_compensated.ckpt'"
 
--- Mol-LLM/bashes/MT_from-string_only_12ep_string+graph_6ep_0502.sh
+++ Mol-LLM/bashes/MT_from-string_only_12ep_string+graph_6ep_0502.sh
@@ -11,7 +11,7 @@ trainer=mistral7b_80gb \
 trainer.max_epochs=${max_epochs} \
 trainer.mol_representation=string+graph \
 trainer.skip_sanity_check=false \
-pretrained_ckpt_path="'/data/all_checkpoints/MT_from-string_only_qformer-gine_tokengt_pretraining_1ep_0501/epoch=00-step=3415.ckpt'" \
+pretrained_ckpt_path="'/data/all_checkpoints/MT_from-string_only_qformer-gine_tokengt_pretraining_1ep_0501/epoch=00-step=3415_lora_compensated.ckpt'" \
 trainer.init_lr=0.00004 \
 trainer.warmup_lr=0.000004 \
 trainer.val_check_interval=0.20
