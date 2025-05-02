f9fa1347cbb99cd6e7197b35a1629e686b00760c
chless <chlessling@gmail.com>
2025-05-02 08:45:05 +0000

    update exp bash

--- Mol-LLM/bashes/MT_from-string_only_12ep_molpo-wo-L-rej-margin-clip_6ep_0502.sh
+++ Mol-LLM/bashes/MT_from-string_only_12ep_molpo-wo-L-rej-margin-clip_6ep_0502.sh
@@ -1,6 +1,6 @@
 export TOKENIZERS_PARALLELISM=false;
 gpus="'0,1,2,3,4,5,6,7'"
-margin_clip_scale=0.4
+margin_clip_scale=1.0
 file_name=MT_from-string_only_12ep_molpo-wo-L-rej-margin_clip_scale-${margin_clip_scale}_6ep_0502
 
 python Mol-LLM/stage3.py \
