1ada1fee578d438107e0c8d9b3138e0c2fc9e0fa
chless <chlessling@gmail.com>
2025-05-09 13:46:32 +0000

    add test bash

---
+++ Mol-LLM/bashes/molpo_model_test.sh
@@ -0,0 +1,16 @@
+export TOKENIZERS_PARALLELISM=false;
+gpus=$1
+data_tag=$2
+filename=test_${data_tag}_molpo_0509
+ckpt_path="'/data/all_checkpoints/MT_from-string_only_12ep_molpo-wo-L-rej-margin_clip_scale-1.0_6ep_0502/epoch=05-step=8397.ckpt'"
+
+echo "==============Executing task: ablation==============="
+python Mol-LLM/stage3.py \
+trainer.devices=$gpus \
+mode=test \
+filename=${filename} \
+data.data_tag=${data_tag} \
+trainer=mistral7b_80gb \
+trainer.skip_sanity_check=false \
+ckpt_path=${ckpt_path}
+
