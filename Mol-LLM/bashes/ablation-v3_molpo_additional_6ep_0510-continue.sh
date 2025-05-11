b65056099e6dfce65e3ebe822424f4cb72e1081f
chless <chlessling@gmail.com>
2025-05-11 01:33:23 +0000

    update exp bash

--- Mol-LLM/bashes/ablation-v3_molpo_additional_6ep_0510-continue.sh
+++ Mol-LLM/bashes/ablation-v3_molpo_additional_6ep_0510-continue.sh
@@ -2,8 +2,8 @@ export TOKENIZERS_PARALLELISM=false;
 gpus="'0,1,2,3,4,5,6,7'"
 max_epochs=6
 total_batch_size=512
-filename=ablation-v3_molpo_additional_6ep_0510-2
-pretrained_ckpt_path="'/data/all_checkpoints/ablation-v3_molpo_additional_6ep_0510/epoch=05-step=13157.ckpt'"
+filename=ablation-v3_molpo_additional_6ep_0510-continue-clip-20.0
+pretrained_ckpt_path="'/data/all_checkpoints/ablation-v3_molpo_additional_6ep_0510/epoch=05-step=6581.ckpt'"
 
 echo "==============Executing task: ablation==============="
 python Mol-LLM/stage3.py \
@@ -17,4 +17,5 @@ trainer.total_batch_size=${total_batch_size} \
 pretrained_ckpt_path=${pretrained_ckpt_path} \
 trainer.min_lr=0.000005 \
 trainer.init_lr=0.00001 \
-trainer.warmup_lr=0.000001
+trainer.warmup_lr=0.000001 \
+trainer.margin_clip_scale=20.0
